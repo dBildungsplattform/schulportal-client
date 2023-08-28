d235125da68ac4679f5585d4420ca4d65bb65700
jaggarnaut <julian.aggarwal@dataport.de>
2023-08-28 16:59:07 +0200

    installed and configured vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,42 @@
+import 'vuetify/styles'
+import { createVuetify, type VuetifyOptions } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const shTheme = {
+    dark: false,
+    colors: {
+      background: '#FFFFFF',
+      surface: '#FFFFFF',
+      primary: '#6200EE',
+      'primary-darken-1': '#3700B3',
+      secondary: '#03DAC6',
+      'secondary-darken-1': '#018786',
+      error: '#B00020',
+      info: '#2196F3',
+      success: '#4CAF50',
+      warning: '#FB8C00',
+    },
+  }
+
+const vuetifyConfig: VuetifyOptions = {
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  },
+  theme: {
+    defaultTheme: 'shTheme',
+    themes: {
+      shTheme
+    }
+  }
+}
+
+export default createVuetify(vuetifyConfig)
