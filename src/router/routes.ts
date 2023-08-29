f40895bdb6776925709d72f8037863fcbad2a43e
jaggarnaut <julian.aggarwal@dataport.de>
2023-08-29 08:53:46 +0200

    outsourced routes to separate file

---
+++ src/router/routes.ts
@@ -0,0 +1,17 @@
+/* home is the only route we won't lazy-load, all other routes are loaded when visited */
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  }
+]
+
+export default routes
