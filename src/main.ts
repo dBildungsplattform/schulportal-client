2d474c454b14ad764f908b4b6be215db855e5398
jaggarnaut <julian.aggarwal@dataport.de>
2023-08-31 08:57:23 +0200

    installed pinia

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import App from './App.vue'
 import i18n from './plugins/i18n'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import pinia from './plugins/pinia'
 
 import './assets/main.css'
 
 createApp(App)
   .use(i18n)
+  .use(pinia)
   .use(router)
   .use(vuetify)
   .mount('#app')
