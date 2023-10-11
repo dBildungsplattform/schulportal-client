66b4797bae1b61128796c05599f296a870d28d4a
jaggarnaut <julian.aggarwal@dataport.de>
2023-10-11 13:31:15 +0200

    enabled vuetify treeshaking

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,5 @@
 import 'vuetify/styles'
 import { createVuetify, type VuetifyOptions } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
@@ -22,8 +20,6 @@ const shTheme = {
   }
 
 const vuetifyConfig: VuetifyOptions = {
-  components,
-  directives,
   icons: {
     defaultSet: 'mdi',
     aliases,
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify()
   ],
   resolve: {
     alias: {
