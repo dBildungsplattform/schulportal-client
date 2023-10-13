f7d9f49d578d6e31616f3faad7122874a51e3e81
jaggarnaut <julian.aggarwal@dataport.de>
2023-10-13 13:33:28 +0200

    added provider cards to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,31 @@
+<script setup lang="ts">
+  import { useProviderStore } from '@/stores/ProviderStore'
+
+  const providerStore = useProviderStore()
+  providerStore.getAllProviders()
+  const providers = providerStore.allProviders
+</script>
+
 <template>
-  <div class="dashboard">
+  <div class="home">
     <h1>This is gonna be the home page</h1>
+    <v-row>
+      <v-col
+        v-for="provider in providers"
+        :key="provider.id"
+        cols="12"
+      >
+        <a :href="provider.url">
+          <v-card
+            :title="provider.name"
+          >
+          </v-card>
+        </a>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
+  
 </style>
