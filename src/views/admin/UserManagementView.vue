33f536eb28f2ca947e70ab215e8d9358b36a3cab
jaggarnaut <julian.aggarwal@dataport.de>
2023-10-25 16:11:24 +0200

    added user edit dialog

--- src/views/admin/UserManagementView.vue
+++ src/views/admin/UserManagementView.vue
@@ -14,24 +14,60 @@
   type ReadonlyDataTableHeader = UnwrapReadonlyArrayType<DT['headers']>
 
   const { t } = useI18n({ useScope: 'global' })
-  const itemsPerPage = 25
   const headers = [
-    { title: t('user.name'), key: 'person.name', align: 'start' }
+    { title: t('user.name'), key: 'person.name', align: 'start' },
+    { title: t('action'), key: 'actions', sortable: false }
   ] as ReadonlyDataTableHeader[]
+  const itemsPerPage = 25
 </script>
 
 <template>
   <div class="admin">
     <h2>{{ $t('admin.user.management') }}</h2>
     <v-data-table
-      v-model:items-per-page="itemsPerPage"
+      class="elevation-1"
+      data-testid="user-table"
       :headers="headers"
       :items="personStore.allPersons"
-      class="elevation-1"
+      v-model:items-per-page="itemsPerPage"
     >
       <template #[`item.person.name`]="{ item }"
         >{{ item.raw.person.name.vorname }} {{ item.raw.person.name.familienname }}</template
       >
+      <template #[`item.actions`]="{ item }">
+        <v-dialog
+          persistent
+          width="500"
+        >
+          <template v-slot:activator="{ props }">
+            <v-icon
+              class="me-2"
+              data-testid="user-actions-icon"
+              size="small"
+              v-bind="props"
+            >
+              mdi-pencil
+            </v-icon>
+          </template>
+
+          <template v-slot:default="{ isActive }">
+            <v-card>
+              <v-card-text>
+                {{ item }}
+              </v-card-text>
+              <v-card-actions>
+                <v-btn
+                  block
+                  @click.stop="isActive.value = false"
+                  color="primary"
+                  data-testid="close-user-edit-dialog-button"
+                  >{{ $t('close') }}</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
+      </template>
     </v-data-table>
   </div>
 </template>
