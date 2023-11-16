6cd8b713ff2c9a0ce2dcfbfa635ce2c7386bb57e
DPDS93CT <cornelius.tegtmeyer@dataport.de>
2023-11-16 15:29:35 +0100

    enable xforwarding for secure cookies

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:9091/',
         changeOrigin: true,
-        secure: true
+        secure: false,
+        xfwd: true,
       }
     }
   },
