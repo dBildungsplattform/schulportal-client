823f990a13334e22461754a85e74ea52d84d5ad1
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2023-12-18 15:09:59 +0100

    make VueI18n CSP compliant

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineConfig({
-  plugins: [vue(), vuetify(), basicSsl()],
+  plugins: [VueI18nPlugin({}), vue(), vuetify(), basicSsl()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -35,6 +36,21 @@ export default defineConfig({
     setupFiles: 'vitest.setup.ts',
     coverage: {
       reporter: ['text', 'lcov', 'html']
+    }
+  },
+  preview: {
+    port: 8099,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9091/',
+        changeOrigin: true,
+        secure: false,
+        xfwd: true
+      }
+    },
+    headers: {
+      'Content-Security-Policy':
+        "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self'; frame-src 'self';"
     }
   }
 })
