21508c13e71cb9ddfa6a0bba35ed73b7692c0b5b
jaggarnaut <julian.aggarwal@dataport.de>
2024-01-16 16:21:41 +0100

    enabled csp compliant vue-i18n interpolation

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,16 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineConfig({
-  plugins: [VueI18nPlugin({}), vue(), vuetify(), basicSsl()],
+  plugins: [
+    VueI18nPlugin({
+      /* we have to enable jit compilation to use i18n interpolation without violating the CSP
+         https://github.com/intlify/vue-i18n-next/issues/1059#issuecomment-1646097462 */
+      jitCompilation: true
+    }),
+    vue(),
+    vuetify(),
+    basicSsl()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
