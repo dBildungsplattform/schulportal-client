0c6f40abb3fd6a0702218e515d9affbc6754d7b4
DPDS93CT <cornelius.tegtmeyer@dataport.de>
2024-01-31 10:36:16 +0100

    add debugging for vite-proxy temp

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,18 @@ export default defineConfig({
         target: 'http://localhost:9090/',
         changeOrigin: true,
         secure: false,
-        xfwd: true
+        xfwd: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
       }
     }
   },
