41ec905ead0331df6647d36b9a8746f7afe8c43b
godtierbatuhan <boucharay1001@gmail.com>
2024-02-16 13:50:59 +0100

    Test für Store.

---
+++ src/stores/OrganisationStore.spec.ts
@@ -0,0 +1,63 @@
+import { useOrganisationStore, type OrganisationStore } from './OrganisationStore'
+import ApiService from '@/services/ApiService'
+import MockAdapter from 'axios-mock-adapter'
+import { setActivePinia, createPinia } from 'pinia'
+import { type OrganisationResponse, OrganisationResponseTypEnum } from '../api-client/generated/api'
+
+const mockadapter: MockAdapter = new MockAdapter(ApiService)
+
+describe('OrganisationStore', () => {
+  let organisationStore: OrganisationStore
+  beforeEach(() => {
+    setActivePinia(createPinia())
+    organisationStore = useOrganisationStore()
+    mockadapter.reset()
+  })
+
+  it('should initialize state correctly', () => {
+    expect(organisationStore.AllOrganisations).toEqual([])
+    expect(organisationStore.errorCode).toEqual('')
+    expect(organisationStore.loading).toBeFalsy()
+  })
+
+  describe('getAllOrganisations', () => {
+    it('should fetch all organisations and update state', async () => {
+      const mockResponse: OrganisationResponse[] = [
+        {
+          id: '1',
+          kennung: 'Org1',
+          name: 'Organisation 1',
+          namensergaenzung: 'Ergänzung',
+          kuerzel: 'O1',
+          typ: OrganisationResponseTypEnum.Anbieter
+        }
+      ]
+
+      mockadapter.onGet('/api/organisationen').replyOnce(200, mockResponse)
+      const getAllOrgaisationsPromise: Promise<void> = organisationStore.getAllOrganisations()
+      await getAllOrgaisationsPromise
+      expect(organisationStore.AllOrganisations).toEqual(mockResponse)
+      expect(organisationStore.loading).toBeFalsy()
+    })
+
+    it('should handle string error', async () => {
+      mockadapter.onGet('/api/organisationen').replyOnce(500, 'some mock server error')
+      const getAllOrgaisationsPromise: Promise<void> = organisationStore.getAllOrganisations()
+      expect(organisationStore.loading).toBe(true)
+      await getAllOrgaisationsPromise
+      expect(organisationStore.AllOrganisations).toEqual([])
+      expect(organisationStore.errorCode).toEqual('UNSPECIFIED_ERROR')
+      expect(organisationStore.loading).toBe(false)
+    })
+
+    it('should handle error code', async () => {
+      mockadapter.onGet('/api/organisationen').replyOnce(500, { code: 'some mock server error' })
+      const getAllOrgaisationsPromise: Promise<void> = organisationStore.getAllOrganisations()
+      expect(organisationStore.loading).toBe(true)
+      await getAllOrgaisationsPromise
+      expect(organisationStore.AllOrganisations).toEqual([])
+      expect(organisationStore.errorCode).toEqual('some mock server error')
+      expect(organisationStore.loading).toBe(false)
+    })
+  })
+})
