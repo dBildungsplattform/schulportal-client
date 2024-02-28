79a25a9484bd4f19463cf4f930f503b4a6b42c1c
jaggarnaut <julian.aggarwal@dataport.de>
2024-02-28 15:42:38 +0100

    fixed test

--- src/components/form/InputRow.spec.ts
+++ src/components/form/InputRow.spec.ts
@@ -1,50 +1,8 @@
 import { expect, test } from 'vitest'
 import { VueWrapper, mount } from '@vue/test-utils'
 import InputRow from './InputRow.vue'
-import { useForm, type BaseFieldProps, type TypedSchema } from 'vee-validate'
-import { type Ref } from 'vue'
-import type {
-  CreateRolleBodyParamsMerkmaleEnum,
-  CreateRolleBodyParamsRollenartEnum
-} from '@/stores/RolleStore'
-import { object, string } from 'yup'
-import { toTypedSchema } from '@vee-validate/yup'
 
 let wrapper: VueWrapper | null = null
-
-const vuetifyConfig = (state: {
-  errors: Array<string>
-}): { props: { error: boolean; 'error-messages': Array<string> } } => ({
-  props: {
-    error: !!state.errors.length,
-    'error-messages': state.errors
-  }
-})
-
-type RolleCreationForm = {
-  selectedSchulstrukturknoten: string
-  selectedRollenArt: CreateRolleBodyParamsRollenartEnum
-  selectedRollenName: string
-  selectedMerkmale: CreateRolleBodyParamsMerkmaleEnum[]
-}
-
-const validationSchema: TypedSchema = toTypedSchema(
-  object({
-    selectedRollenName: string()
-      .max(200, 'Rollenname darf maximal 200 Zeichen lang sein')
-      .required('Rollenname ist ein Pflichtfeld')
-  })
-)
-
-// eslint-disable-next-line @typescript-eslint/typedef
-const { defineField } = useForm<RolleCreationForm>({
-  validationSchema: validationSchema
-})
-
-const [testModel, testModelProps]: [
-  Ref<string>,
-  Ref<BaseFieldProps & { error: boolean; 'error-messages': Array<string> }>
-] = defineField('selectedSchulstrukturknoten', vuetifyConfig)
 
 beforeEach(() => {
   document.body.innerHTML = `
@@ -56,12 +14,9 @@ beforeEach(() => {
   wrapper = mount(InputRow, {
     attachTo: document.getElementById('app') || '',
     props: {
-      id: 'test-input',
+      labelForId: 'test-input',
       errorLabel: "i'm an error",
-      fieldProps: testModelProps,
-      label: 'what a label',
-      modelValue: testModel.value,
-      placeholder: 'holding the place'
+      label: 'what a label'
     },
     global: {
       components: {
