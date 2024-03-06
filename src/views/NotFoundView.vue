07cdbec02cb3f13041fc36839591b7e1ffc5f278
Caspar Neumann <caspar.neumann@capgemini.com>
2024-03-06 12:55:47 +0100

    Use SpshAlert & i18n

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,32 +1,21 @@
 <script setup lang="ts">
+  import SpshAlert from '@/components/alert/SpshAlert.vue';
   function goBack(): void {
     window.history.back();
   }
 </script>
 
 <template>
-  <div class="not-found-container">
-    <h1>404 Nicht gefunden</h1>
-    <p>Die aufgerufene Seite wurde nicht gefunden</p>
-    <button @click="goBack">Zurück</button>
-  </div>
+  <SpshAlert
+    :modelValue="true"
+    :title="404"
+    :type="'error'"
+    :closable="false"
+    :showButton="true"
+    :buttonText="$t('nav.back')"
+    :buttonAction="goBack"
+    :text="$t('errors.NOT_FOUND')"
+  />
 </template>
 
-<style scoped>
-  .not-found-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    min-height: 60vh;
-    text-align: center;
-  }
-
-  h1 {
-    margin-bottom: 1rem;
-  }
-
-  p {
-    margin-bottom: 2rem;
-  }
-</style>
+<style scoped></style>
