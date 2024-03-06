3cca5e3abc431e7ff0029b0b04c57c424fb5a612
godtierbatuhan <boucharay1001@gmail.com>
2024-03-06 21:17:22 +0100

    Fixed transform pixelization

--- src/components/layout/ServiceProviderCategory.vue
+++ src/components/layout/ServiceProviderCategory.vue
@@ -8,32 +8,34 @@
   }>();
 </script>
 
-<template v-if="serviceProvider && serviceProviders.length > 0">
-  <v-row>
-    <label class="mx-3">{{ categoryTitle }}</label>
-    <v-col>
-      <v-divider
-        class="border-opacity-100 rounded"
-        color="#E5EAEF"
-        thickness="6"
-      ></v-divider>
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col
-      v-for="serviceProvider in serviceProviders"
-      :key="serviceProvider.id"
-      cols="12"
-      md="6"
-      lg="4"
-    >
-      <ServiceProviderCard
-        :href="serviceProvider.url"
-        :newTab="true"
-        :testId="`service-provider-card-${serviceProvider.id}`"
-        :title="serviceProvider.name"
-        :logoUrl="serviceProvider.logoUrl"
-      ></ServiceProviderCard>
-    </v-col>
-  </v-row>
+<template>
+  <v-container v-if="serviceProviders?.length > 0">
+    <v-row>
+      <label class="mx-3">{{ categoryTitle }}</label>
+      <v-col>
+        <v-divider
+          class="border-opacity-100 rounded"
+          color="#E5EAEF"
+          thickness="6"
+        ></v-divider>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        v-for="serviceProvider in serviceProviders"
+        :key="serviceProvider.id"
+        cols="12"
+        md="6"
+        lg="4"
+      >
+        <ServiceProviderCard
+          :href="serviceProvider.url"
+          :newTab="true"
+          :testId="`service-provider-card-${serviceProvider.id}`"
+          :title="serviceProvider.name"
+          :logoUrl="serviceProvider.logoUrl"
+        ></ServiceProviderCard>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
