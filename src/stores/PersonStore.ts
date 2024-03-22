46a5bc0914d939e53a503d57e372a6626291bca0
Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
2024-03-22 11:39:02 +0100

    Spsh 411 extend person creation form (#91)
    * componentized form layout
    
    * lint and format fixes
    
    * manually emit dirty states when models change and catch them
    
    * fixed tests for build
    
    * fixed rolle validation
    
    * used layout card for vue routing alert
    
    * lint fixes
    
    * fixed tests
    
    * added form component to person creation
    
    * fixed local coverage output
    
    * get all organisations and show dropdown
    
    * prepare personenkontext creation
    
    * removed code for saving personenkontexte
    
    * added rolle select to form and implemented save logic
    
    * reverted changes for testing purposes and ran linter
    
    * review comms
    
    * more review comms (PO)
    
    * lint fixes
    
    * lint fixes
    
    * removed old files
    
    * fixed error in spsh alert
    
    * require rolle and organisationsebene and show both in result overview
    
    * removed unnecessary class
    
    * use name for rolle and organisation after creation
    
    * error message for personenkontext
    
    * lint
    
    * npm audit fix
    
    * sort rollen
    
    * added missing test

--- src/stores/PersonStore.ts
+++ src/stores/PersonStore.ts
@@ -1,9 +1,13 @@
 import { defineStore, type Store, type StoreDefinition } from 'pinia';
 import { isAxiosError, type AxiosResponse } from 'axios';
 import {
+  DbiamPersonenkontexteApiFactory,
   PersonenApiFactory,
   PersonenFrontendApiFactory,
   type CreatePersonBodyParams,
+  type DBiamCreatePersonenkontextBodyParams,
+  type DbiamPersonenkontexteApiInterface,
+  type DBiamPersonenkontextResponse,
   type PersonenApiInterface,
   type PersonendatensatzResponse,
   type PersonenFrontendApiInterface,
@@ -13,6 +17,11 @@ import axiosApiInstance from '@/services/ApiService';
 
 const personenApi: PersonenApiInterface = PersonenApiFactory(undefined, '', axiosApiInstance);
 const personenFrontendApi: PersonenFrontendApiInterface = PersonenFrontendApiFactory(undefined, '', axiosApiInstance);
+const personenKontexteApi: DbiamPersonenkontexteApiInterface = DbiamPersonenkontexteApiFactory(
+  undefined,
+  '',
+  axiosApiInstance,
+);
 
 export type Person = {
   id: string;
@@ -24,6 +33,7 @@ export type Person = {
 };
 
 export type CreatedPerson = CreatePersonBodyParams;
+export type CreatedPersonenkontext = DBiamCreatePersonenkontextBodyParams;
 
 type Personenkontext = {
   id: string;
@@ -34,9 +44,12 @@ export type Personendatensatz = {
   personenkontexte: Array<Personenkontext>;
 };
 
+export type { PersonendatensatzResponse };
+
 type PersonState = {
   allPersons: Array<Personendatensatz>;
   createdPerson: PersonendatensatzResponse | null;
+  createdPersonenkontext: DBiamPersonenkontextResponse | null;
   errorCode: string;
   loading: boolean;
   totalPersons: number;
@@ -46,6 +59,9 @@ type PersonState = {
 type PersonGetters = {};
 type PersonActions = {
   createPerson: (person: CreatePersonBodyParams) => Promise<PersonendatensatzResponse>;
+  createPersonenkontext: (
+    personenkontext: DBiamCreatePersonenkontextBodyParams,
+  ) => Promise<DBiamPersonenkontextResponse>;
   getAllPersons: () => Promise<void>;
   getPersonById: (personId: string) => Promise<Personendatensatz>;
   resetPassword: (personId: string) => Promise<string>;
@@ -59,6 +75,7 @@ export const usePersonStore: StoreDefinition<'personStore', PersonState, PersonG
     return {
       allPersons: [],
       createdPerson: null,
+      createdPersonenkontext: null,
       errorCode: '',
       loading: false,
       totalPersons: 0,
@@ -71,6 +88,26 @@ export const usePersonStore: StoreDefinition<'personStore', PersonState, PersonG
       try {
         const { data }: { data: PersonendatensatzResponse } = await personenApi.personControllerCreatePerson(person);
         this.createdPerson = data;
+        return data;
+      } catch (error: unknown) {
+        this.errorCode = 'UNSPECIFIED_ERROR';
+        if (isAxiosError(error)) {
+          this.errorCode = error.response?.data.code || 'UNSPECIFIED_ERROR';
+        }
+        return await Promise.reject(this.errorCode);
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    async createPersonenkontext(
+      personenkontext: DBiamCreatePersonenkontextBodyParams,
+    ): Promise<DBiamPersonenkontextResponse> {
+      this.loading = true;
+      try {
+        const { data }: { data: DBiamPersonenkontextResponse } =
+          await personenKontexteApi.dBiamPersonenkontextControllerCreatePersonenkontext(personenkontext);
+        this.createdPersonenkontext = data;
         return data;
       } catch (error: unknown) {
         this.errorCode = 'UNSPECIFIED_ERROR';
