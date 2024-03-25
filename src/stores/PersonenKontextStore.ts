bbbe43eaedf70d4200920c39036d1958bd181995
godtierbatuhan <boucharay1001@gmail.com>
2024-03-25 12:21:10 +0100

    Rolle and Zuordnung colums combined with the person object in the result table with corresponding api call.

---
+++ src/stores/PersonenKontextStore.ts
@@ -0,0 +1,87 @@
+import { defineStore, type Store, type StoreDefinition } from 'pinia';
+import { isAxiosError } from 'axios';
+import {
+  DbiamPersonenuebersichtApiFactory,
+  type DBiamCreatePersonenkontextBodyParams,
+  type DbiamPersonenuebersichtApiInterface,
+  type DBiamPersonenuebersichtResponse,
+  type PagedResponse,
+  type SystemrechtResponse,
+} from '../api-client/generated/api';
+import axiosApiInstance from '@/services/ApiService';
+
+const personenuebersichtApi: DbiamPersonenuebersichtApiInterface = DbiamPersonenuebersichtApiFactory(
+  undefined,
+  '',
+  axiosApiInstance,
+);
+
+type PersonenkontextState = {
+  currentPersonenubersicht: DBiamPersonenuebersichtResponse[] | null;
+  allUebersichte: Array<PagedResponse>; 
+  errorCode: string;
+  loading: boolean;
+};
+
+type PersonenkontextGetters = {};
+type PersonenkontextActions = {
+  getPersonenuebersichtById: (personId: string) => Promise<void>;
+  getAllPersonenuebersichte: () => Promise<void>;
+};
+
+export type { SystemrechtResponse, PagedResponse };
+export type CreatedPersonenkontext = DBiamCreatePersonenkontextBodyParams;
+
+export type PersonenkontextStore = Store<
+  'personenkontextStore',
+  PersonenkontextState,
+  PersonenkontextGetters,
+  PersonenkontextActions
+>;
+
+export const usePersonenkontextStore: StoreDefinition<
+  'personenkontextStore',
+  PersonenkontextState,
+  PersonenkontextGetters,
+  PersonenkontextActions
+> = defineStore({
+  id: 'personenkontextStore',
+  state: (): PersonenkontextState => {
+    return {
+      currentPersonenubersicht: null,
+      allUebersichte: [],
+      errorCode: '',
+      loading: false,
+    };
+  },
+  actions: {
+    async getPersonenuebersichtById(personId: string): Promise<void> {
+      this.loading = true;
+      try {
+        const { data }: { data: DBiamPersonenuebersichtResponse[] } =
+          await personenuebersichtApi.dBiamPersonenuebersichtControllerFindPersonenuebersichtenByPerson(personId);
+        this.currentPersonenubersicht = data;
+      } catch (error: unknown) {
+        this.errorCode = 'UNSPECIFIED_ERROR';
+        if (isAxiosError(error)) {
+          this.errorCode = error.response?.data.code || 'UNSPECIFIED_ERROR';
+        }
+      }
+      this.loading = false;
+    },
+    async getAllPersonenuebersichte(): Promise<void> {
+      this.loading = true;
+      try {
+        const { data }: { data: PagedResponse[] } =
+          await personenuebersichtApi.dBiamPersonenuebersichtControllerFindPersonenuebersichten();
+        this.allUebersichte = data;
+      } catch (error: unknown) {
+        this.errorCode = 'UNSPECIFIED_ERROR';
+        if (isAxiosError(error)) {
+          this.errorCode = error.response?.data.code || 'UNSPECIFIED_ERROR';
+        }
+      }
+      this.loading = false;
+    },
+  },
+});
--- src/views/admin/PersonManagementView.vue
+++ src/views/admin/PersonManagementView.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
   import { usePersonStore, type PersonStore, type Personendatensatz } from '@/stores/PersonStore';
-  import { onMounted } from 'vue';
+  import { computed, onMounted } from 'vue';
   import ResultTable from '@/components/admin/ResultTable.vue';
   import { type Composer, useI18n } from 'vue-i18n';
   import type { VDataTableServer } from 'vuetify/lib/components/index.mjs';
 
   import { type Router, useRouter } from 'vue-router';
+  import { usePersonenkontextStore, type PersonenkontextStore } from '@/stores/PersonenKontextStore';
+  import type { PagedResponse } from '@/api-client/generated';
 
   const personStore: PersonStore = usePersonStore();
+  const personenKontextStore: PersonenkontextStore = usePersonenkontextStore();
 
   const { t }: Composer = useI18n({ useScope: 'global' });
 
@@ -16,16 +19,57 @@
     { title: t('person.lastName'), key: 'person.name.familienname', align: 'start' },
     { title: t('person.firstName'), key: 'person.name.vorname', align: 'start' },
     { title: t('person.userName'), key: 'person.referrer', align: 'start' },
+    { title: t('person.rolle'), key: 'person.rolle', align: 'start' },
+    { title: t('person.zuordnung'), key: 'person.administrationsebenen', align: 'start' },
   ];
 
+  type PersonenWithRolleAndZuordnung = {
+    rollen: string;
+    administrationsebenen: string;
+    person: {
+      id: string;
+      name: {
+        familienname: string;
+        vorname: string;
+      };
+      referrer: string;
+    };
+    personenkontexte: {
+      id: string;
+    }[];
+  };
   const router: Router = useRouter();
 
   function navigateToPersonDetails(_$event: PointerEvent, { item }: { item: Personendatensatz }): void {
     router.push({ name: 'person-details', params: { id: item.person.id } });
   }
 
+  // Maps over allPersons, finds the corresponding zuordnungen for each person by matching the personId, and then extracts and combines
+  // the rolle values into a single string. The result is merged with the original person data, adding a new role property.
+  const personenWithUebersicht: PersonenWithRolleAndZuordnung = computed(() => {
+    return personStore.allPersons.map((person: Personendatensatz) => {
+      const uebersicht:
+        | {
+            items: string[];
+          }
+        | undefined = personenKontextStore.allUebersichte.find(
+        (ueb: PagedResponse) => ueb.personId === person.person.id,
+      );
+      // TODO: This needs to be fixed from the backend as the endpoint gives us an array of strings back instead of an array of objects
+      const rollen: string = uebersicht ? uebersicht.zuordnungen.map((zuordnung) => zuordnung.rolle).join(', ') : '';
+      // Choose sskDstNr if available, otherwise sskName
+      const administrationsebenen: string = uebersicht
+        ? uebersicht.zuordnungen
+            .map((zuordnung) => (zuordnung.sskDstNr ? zuordnung.sskDstNr : zuordnung.sskName))
+            .join(', ')
+        : '';
+      return { ...person, rolle: rollen, administrationsebenen: administrationsebenen };
+    });
+  });
+
   onMounted(async () => {
     await personStore.getAllPersons();
+    await personenKontextStore.getAllPersonenuebersichte();
   });
 </script>
 
@@ -35,15 +79,37 @@
     <ResultTable
       data-testid="person-table"
       :header="$t('admin.person.management')"
-      :items="personStore.allPersons || []"
+      :items="personenWithUebersicht || []"
       :loading="personStore.loading"
       :headers="headers"
       @onHandleRowClick="navigateToPersonDetails"
       @onUpdateTable="personStore.getAllPersons()"
       :totalItems="personStore.totalPersons"
       item-value-path="person.id"
-    ></ResultTable>
+      ><template v-slot:[`item.rolle`]="{ item }">
+        <div
+          class="ellipsis-wrapper"
+          :title="item.rolle"
+        >
+          {{ item.rolle }}
+        </div> </template
+      ><template v-slot:[`item.administrationsebenen`]="{ item }">
+        <div
+          class="ellipsis-wrapper"
+          :title="item.administrationsebenen"
+        >
+          {{ item.administrationsebenen }}
+        </div>
+      </template></ResultTable
+    >
   </div>
 </template>
 
-<style></style>
+<style>
+  .ellipsis-wrapper {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    max-width: 241px;
+  }
+</style>
