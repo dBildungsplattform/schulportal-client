6a54ce0002dacbf783bf152c626806aa67c46019
jaggarnaut <julian.aggarwal@dataport.de>
2024-05-27 14:59:37 +0200

    added loader

--- src/views/admin/PersonManagementView.vue
+++ src/views/admin/PersonManagementView.vue
@@ -252,7 +252,8 @@
           >
             <template v-slot:prepend-item>
               <v-list-item>
-                <span class="filter-header">{{ 
+                <v-progress-circular indeterminate v-if="organisationStore.loading"></v-progress-circular>
+                <span v-else class="filter-header">{{ 
                   organisationStore.totalOrganisationen == 1
                     ? $t('admin.schule.schuleFound', { total: organisationStore.totalOrganisationen })
                     : $t('admin.schule.schulenFound', { total: organisationStore.totalOrganisationen })
