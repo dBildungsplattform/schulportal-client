d3a8595dc7b5896b007b977ddac1901851741ecf
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2024-07-01 14:25:32 +0200

    load vuetify default i18n messages

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,27 +1,21 @@
 import { createI18n, type DefaultLocaleMessageSchema } from 'vue-i18n';
 import * as deDE from '../locales/de-DE.json';
+// @ts-ignore
+// eslint-disable-next-line import/extensions
+import vuetifyDeMessages from 'vuetify/lib/locale/de.mjs';
 
 function loadLocaleMessages(): { [x: string]: DefaultLocaleMessageSchema } {
   const messages: { [x: string]: DefaultLocaleMessageSchema } = {
-    de: deDE,
+    de: { $vuetify: vuetifyDeMessages, ...deDE },
   };
 
   return messages;
 }
 
-const messages: { [x: string]: DefaultLocaleMessageSchema } = {
-  de: {
-    ...loadLocaleMessages()['de'],
-    $vuetify: {
-      close: 'Schließen',
-      open: 'Öffnen',
-    },
-  },
-};
 
 export default createI18n({
   fallbackLocale: 'de',
   legacy: false,
   locale: 'de',
-  messages,
+  messages: loadLocaleMessages(),
 });
