5d377655a5504dfa183260804eaef2d8e3d4b1df
Marvin Rode <marvin.rode@capgemini.com>
2024-07-02 14:05:44 +0200

    Add error page

---
+++ src/views/UnknownUserErrorView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import SpshAlert from '@/components/alert/SpshAlert.vue';
+  import { useRouter, type Router } from 'vue-router';
+
+  const route: Router = useRouter();
+
+  function backToStart(): void {
+    route.replace('/');
+  }
+</script>
+
+<template>
+  <SpshAlert
+    :modelValue="true"
+    :title="$t('errors.UNKNOWN_USER.title')"
+    :type="'warning'"
+    :closable="false"
+    :showButton="true"
+    :buttonText="$t('nav.backToStart')"
+    :buttonAction="backToStart"
+    :text="$t('errors.UNKNOWN_USER.text')"
+  />
+</template>
+
+<style scoped></style>
