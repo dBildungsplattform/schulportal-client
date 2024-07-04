17be37222593731e98f4e901f91f7b999ca0e95f
Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
2024-07-04 13:30:24 +0200

    spsh 784 translations for dropdown close/open hover texts (#182)
    * configured vuetify and i18n to merge translations
    
    * load vuetify default i18n messages
    
    ---------
    
    Co-authored-by: Philipp Kleybolte <philipp.kleybolte@capgemini.com>

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,4 +1,7 @@
 import { createVuetify, type ThemeDefinition, type VuetifyOptions } from 'vuetify';
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n';
+import { useI18n } from 'vue-i18n';
+import i18n from '@/plugins/i18n';
 import '@mdi/font/css/materialdesignicons.css';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import '@/styles/main.scss';
@@ -29,6 +32,9 @@ const vuetifyConfig: VuetifyOptions = {
     sets: {
       mdi,
     },
+  },
+  locale: {
+    adapter: createVueI18nAdapter({ i18n, useI18n }),
   },
   theme: {
     cspNonce,
