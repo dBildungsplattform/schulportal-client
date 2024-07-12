b27d575147c94900cc388bb79035fe93a26d46d7
Timo K <timo.kleist@capgemini.com>
2024-07-12 16:43:22 +0200

    software token workflow init

--- src/components/cards/two-factor-authentication/TwoFactorAuthenticationSetUp.vue
+++ src/components/cards/two-factor-authentication/TwoFactorAuthenticationSetUp.vue
@@ -4,9 +4,14 @@
   import { useDisplay } from 'vuetify';
   import LayoutCard from '@/components/cards/LayoutCard.vue';
   import SpshTooltip from '@/components/admin/SpshTooltip.vue';
+  import SoftwareTokenWorkflow from './SoftwareTokenWorkflow.vue';
 
   const { mdAndDown }: { mdAndDown: Ref<boolean> } = useDisplay();
   const selectedOption: Ref<'software' | 'hardware'> = ref('software');
+
+  const proceeded: Ref<boolean> = ref(false);
+
+  const dialogHeader = ref('default');
 
   type Props = {
     errorCode: string;
@@ -18,7 +23,17 @@
 
   async function close2FADialog(isActive: Ref<boolean>): Promise<void> {
     isActive.value = false;
-    selectedOption.value = "software";
+    proceeded.value = false;
+    selectedOption.value = 'software';
+    dialogHeader.value = 'default';
+  }
+
+  async function incWorkflow(): Promise<void> {
+    proceeded.value = true;
+  }
+
+  async function handleHeaderUpdate(header: string): Promise<void> {
+    dialogHeader.value = header;
   }
 </script>
 
@@ -52,28 +67,53 @@
     <template v-slot:default="{ isActive }">
       <LayoutCard
         :closable="true"
-        :header="$t('admin.person.twoFactorAuthentication.setUpLong')"
+        :header="dialogHeader"
         @onCloseClicked="close2FADialog(isActive)"
       >
-        <v-card-text>
+        <v-card-text v-if="!proceeded">
           <v-container>
             <v-row class="text-body bold px-md-16">
               <v-col>
                 <v-radio-group v-model="selectedOption">
-                  <v-radio :label="$t('admin.person.twoFactorAuthentication.softwareTokenOption')" value="software"></v-radio>
-                  <v-radio :label="$t('admin.person.twoFactorAuthentication.hardwareTokenOption')" value="hardware"></v-radio>
+                  <v-radio
+                    :label="$t('admin.person.twoFactorAuthentication.softwareTokenOption')"
+                    value="software"
+                  ></v-radio>
+                  <v-radio
+                    :label="$t('admin.person.twoFactorAuthentication.hardwareTokenOption')"
+                    value="hardware"
+                  ></v-radio>
                 </v-radio-group>
               </v-col>
             </v-row>
             <v-row>
               <v-col cols="12">
-                <p class="text-body" v-if="selectedOption === 'software'"> {{ $t('admin.person.twoFactorAuthentication.softwareTokenText') }}</p>
-                <p class="text-body" v-if="selectedOption === 'hardware'"> {{ $t('admin.person.twoFactorAuthentication.hardwareTokenText') }}</p>
+                <p
+                  class="text-body"
+                  v-if="selectedOption === 'software'"
+                >
+                  {{ $t('admin.person.twoFactorAuthentication.softwareTokenText') }}
+                </p>
+                <p
+                  class="text-body"
+                  v-if="selectedOption === 'hardware'"
+                >
+                  {{ $t('admin.person.twoFactorAuthentication.hardwareTokenText') }}
+                </p>
               </v-col>
             </v-row>
           </v-container>
         </v-card-text>
-        <v-card-actions class="justify-center">
+        <v-container v-if="proceeded">
+          <SoftwareTokenWorkflow
+            v-if="selectedOption === 'software'"
+            @updateHeader="handleHeaderUpdate"
+          ></SoftwareTokenWorkflow>
+        </v-container>
+        <v-card-actions
+          class="justify-center"
+          v-if="!proceeded"
+        >
           <v-row class="justify-center">
             <v-col
               cols="12"
@@ -86,7 +126,7 @@
                 @click.stop="close2FADialog(isActive)"
                 data-testid="close-two-way-authentification-dialog-button"
               >
-                {{ $t('cancel')}}
+                {{ $t('cancel') }}
               </v-btn>
             </v-col>
             <v-col
@@ -97,7 +137,9 @@
               <v-btn
                 :block="mdAndDown"
                 class="primary button"
-                data-testid="two-way-authentification-set-up-button"              >
+                @click.stop="incWorkflow()"
+                data-testid="two-way-authentification-set-up-button"
+              >
                 {{ $t('proceed') }}
               </v-btn>
             </v-col>
