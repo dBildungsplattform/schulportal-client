3098a9f3d2b2aca8bc3c61642675dbb659ae47e1
godtierbatuhan <boucharay1001@gmail.com>
2024-07-15 14:02:13 +0200

    Coverage

--- src/views/admin/rollen/RolleDetailsView.spec.ts
+++ src/views/admin/rollen/RolleDetailsView.spec.ts
@@ -4,11 +4,13 @@ import RolleDetailsView from './RolleDetailsView.vue';
 import { setActivePinia, createPinia } from 'pinia';
 import routes from '@/router/routes';
 import { type Router, createRouter, createWebHistory } from 'vue-router';
+import { RollenMerkmal, RollenSystemRecht, useRolleStore, type RolleStore } from '@/stores/RolleStore';
 
 let wrapper: VueWrapper | null = null;
 let router: Router;
+let rolleStore: RolleStore;
 
-beforeEach( async() => {
+beforeEach(async () => {
   setActivePinia(createPinia());
   document.body.innerHTML = `
     <div>
@@ -16,10 +18,24 @@ beforeEach( async() => {
     </div>
   `;
 
+  rolleStore = useRolleStore();
+
   router = createRouter({
     history: createWebHistory(),
     routes,
   });
+
+  rolleStore.updatedRolle = {
+    administeredBySchulstrukturknoten: '1234',
+    rollenart: 'LEHR',
+    name: 'Updated Lehrer',
+    merkmale: ['KOPERS_PFLICHT'] as unknown as Set<RollenMerkmal>,
+    systemrechte: ['ROLLEN_VERWALTEN'] as unknown as Set<RollenSystemRecht>,
+    createdAt: '2022',
+    updatedAt: '2023',
+    id: '1',
+    serviceProviders: [{ id: 'sp1', name: 'ServiceProvider1' }],
+  };
 
   router.push('/');
   await router.isReady();
