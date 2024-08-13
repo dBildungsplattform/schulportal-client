28ff4f3da8e8eed5d933a2757b2c5b4a32bf3c6d
godtierbatuhan <boucharay1001@gmail.com>
2024-08-13 20:04:01 +0200

    lint

--- src/views/admin/rollen/RolleCreationView.vue
+++ src/views/admin/rollen/RolleCreationView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { ref, type Ref, onMounted, onUnmounted, computed, type ComputedRef, watch } from 'vue';
+  import { ref, type Ref, onMounted, onUnmounted, computed, type ComputedRef } from 'vue';
   import {
     useOrganisationStore,
     type OrganisationStore,
