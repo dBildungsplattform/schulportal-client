2f67490a4f7305b1f5a8e699972d5db71ad58f84
Yannick Clausen <yannick.clausen@capgemini.com>
2024-08-19 16:59:48 +0200

    only display date of lock

--- src/views/admin/PersonDetailsView.vue
+++ src/views/admin/PersonDetailsView.vue
@@ -125,9 +125,6 @@
           year: 'numeric',
           month: '2-digit',
           day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
         }).format(new Date(value));
       default:
         return value;
