1d203134d86beabd72ce43292d56ddd56110f058
godtierbatuhan <boucharay1001@gmail.com>
2024-09-27 15:08:52 +0200

    Tests

--- src/stores/PersonStore.spec.ts
+++ src/stores/PersonStore.spec.ts
@@ -7,6 +7,7 @@ import {
   type DBiamPersonenuebersichtResponse,
   type PersonFrontendControllerFindPersons200Response,
   type PersonLockResponse,
+  type PersonMetadataBodyParams,
   type PersonendatensatzResponse,
 } from '@/api-client/generated';
 import { usePersonStore, type PersonStore, type Personendatensatz } from './PersonStore';
@@ -503,4 +504,95 @@ describe('PersonStore', () => {
       expect(personStore.currentPerson).toBe(null);
     });
   });
+
+  describe('changePersonInfoById', () => {
+    it('should update person info and set patchedPerson', async () => {
+      const personId = '1234';
+      const vorname = 'Samuel';
+      const familienname = 'Vimes';
+      const personalnummer = '9876';
+
+      const mockCurrentPerson: PersonendatensatzResponse = {
+        person: {
+          id: personId,
+          name: {
+            familienname: 'Old',
+            vorname: 'Name',
+          },
+          mandant: '',
+          geburt: {},
+          stammorganisation: '',
+          geschlecht: '',
+          lokalisierung: '',
+          vertrauensstufe: Vertrauensstufe.Teil,
+          revision: '1',
+          startpasswort: '',
+          lastModified: '2099-12-22',
+          
+        },
+      };
+
+      personStore.currentPerson = mockCurrentPerson;
+
+      const mockResponse: PersonendatensatzResponse = {
+        person: {
+          id: personId,
+          name: {
+            familienname,
+            vorname,
+          },
+          personalnummer,
+          revision: '2',
+          lastModified: '2024-01-02',
+        },
+      };
+
+      const expectedBodyParams: PersonMetadataBodyParams = {
+        vorname,
+        familienname,
+        personalnummer,
+        revision: '1',
+        lastModified: '2024-01-01',
+      };
+
+      mockadapter.onPatch(`/api/personen/${personId}/metadata`).reply((config) => {
+        expect(JSON.parse(config.data)).toEqual(expectedBodyParams);
+        return [200, mockResponse];
+      });
+
+      await personStore.changePersonInfoById(personId, vorname, familienname, personalnummer);
+
+      expect(personStore.loading).toBe(false);
+      expect(personStore.patchedPerson).toEqual(mockResponse);
+      expect(personStore.errorCode).toEqual('');
+    });
+
+    it('should handle error and set errorCode', async () => {
+      const personId: string = '1234';
+      const vorname: string = 'Samuel';
+      const familienname: string = 'Vimes';
+
+      mockadapter.onPatch(`/api/personen/${personId}/metadata`).reply(500, { i18nKey: 'ERROR_UPDATING_USER' });
+
+      await personStore.changePersonInfoById(personId, vorname, familienname);
+
+      expect(personStore.loading).toBe(false);
+      expect(personStore.errorCode).toEqual('ERROR_UPDATING_USER');
+    });
+
+    it('should handle unknown error', async () => {
+      const personId: string = '1234';
+      const vorname: string = 'Samuel';
+      const familienname: string = 'Vimes';
+
+      mockadapter.onPatch(`/api/personen/${personId}/metadata`).reply(500, 'Unknown error');
+
+      await personStore.changePersonInfoById(personId, vorname, familienname);
+
+      expect(personStore.loading).toBe(false);
+      expect(personStore.errorCode).toEqual('ERROR_LOADING_USER');
+    });
+  });
+
+  
 });
