a12d0d75a29824be372262b42176ce5567fb9503
jaggarnaut <julian.aggarwal@dataport.de>
2024-10-22 13:54:45 +0200

    added rollen select

--- src/views/admin/PersonImportView.vue
+++ src/views/admin/PersonImportView.vue
@@ -2,8 +2,10 @@
   import FormRow from '@/components/form/FormRow.vue';
   import LayoutCard from '@/components/cards/LayoutCard.vue';
   import { useRouter, type Router } from 'vue-router';
+  import { useRollen } from '@/composables/useRollen';
+  import { usePersonenkontextStore, type PersonenkontextStore } from '@/stores/PersonenkontextStore';
   import { useSchulen } from '@/composables/useSchulen';
-  import { onMounted, ref, type ComputedRef, type Ref } from 'vue';
+  import { onMounted, ref, watch, type ComputedRef, type Ref } from 'vue';
   import type { TranslatedObject } from '@/types';
   import { useForm, type BaseFieldProps, type FormContext, type TypedSchema } from 'vee-validate';
   import { toTypedSchema } from '@vee-validate/yup';
@@ -12,12 +14,17 @@
   import { OrganisationsTyp, useOrganisationStore, type OrganisationStore } from '@/stores/OrganisationStore';
 
   const organisationStore: OrganisationStore = useOrganisationStore();
+  const personenkontextStore: PersonenkontextStore = usePersonenkontextStore();
+
   const router: Router = useRouter();
   const { t }: Composer = useI18n({ useScope: 'global' });
 
+  const rollen: ComputedRef<TranslatedObject[] | undefined> = useRollen();
   const schulen: ComputedRef<TranslatedObject[] | undefined> = useSchulen();
   const searchInputSchule: Ref<string> = ref('');
   const hasAutoselectedSchule: Ref<boolean> = ref(false);
+
+  let timerId: ReturnType<typeof setTimeout>;
 
   const validationSchema: TypedSchema = toTypedSchema(
     object({
@@ -35,7 +42,8 @@
   });
 
   type PersonImportForm = {
-    selectedOrganisation: string;
+    selectedSchule: string;
+    selectedRolle: string;
   };
 
   const formContext: FormContext<PersonImportForm, PersonImportForm> = useForm<PersonImportForm>({
@@ -45,7 +53,50 @@
   const [selectedSchule, selectedSchuleProps]: [
     Ref<string | undefined>,
     Ref<BaseFieldProps & { error: boolean; 'error-messages': Array<string> }>,
-  ] = formContext.defineField('selectedOrganisation', vuetifyConfig);
+  ] = formContext.defineField('selectedSchule', vuetifyConfig);
+
+  const [selectedRolle, selectedRolleProps]: [
+    Ref<string | undefined>,
+    Ref<BaseFieldProps & { error: boolean; 'error-messages': Array<string> }>,
+  ] = formContext.defineField('selectedRolle', vuetifyConfig);
+
+  watch(selectedSchule, (newValue: string | undefined, oldValue: string | undefined) => {
+    if (newValue && newValue !== oldValue) {
+      // Fetch the roles after selecting the organization
+      personenkontextStore.processWorkflowStep({
+        organisationId: newValue,
+        limit: 25,
+        rolleName: 'SuS',
+      });
+
+      // Reset the selectedRolle field only if oldValue was not undefined
+      if (oldValue !== undefined) {
+        selectedRolle.value = undefined;
+      }
+    } else if (!newValue) {
+      // If the organization is cleared, reset selectedRolle
+      selectedRolle.value = undefined;
+    }
+  });
+
+  watch(searchInputSchule, (newValue: string | undefined, _oldValue: string | undefined) => {
+    /* cancel pending call */
+    clearTimeout(timerId);
+
+    /* delay new call 500ms */
+    timerId = setTimeout(() => {
+      organisationStore.getAllOrganisationen({
+        searchString: newValue,
+        excludeTyp: [OrganisationsTyp.Klasse],
+        limit: 30,
+        systemrechte: ['PERSONEN_VERWALTEN', 'IMPORT_DURCHFUEHREN'],
+      });
+    }, 500);
+  });
+
+  function clearSelectedRolle(): void {
+    selectedRolle.value = undefined;
+  }
 
   function clearSelectedSchule(): void {
     selectedSchule.value = undefined;
@@ -56,7 +107,11 @@
   }
 
   onMounted(async () => {
-    await organisationStore.getAllOrganisationen({ includeTyp: OrganisationsTyp.Schule, limit: 30 });
+    await organisationStore.getAllOrganisationen({
+      includeTyp: OrganisationsTyp.Schule,
+      systemrechte: ['PERSONEN_VERWALTEN', 'IMPORT_DURCHFUEHREN'],
+      limit: 30,
+    });
     organisationStore.errorCode = '';
   });
 </script>
@@ -79,6 +134,7 @@
     >
       <v-container class="px-3 px-sm-16">
         <v-container class="px-lg-16">
+          <!-- Schulauswahl -->
           <FormRow
             :errorLabel="selectedSchuleProps['error']"
             :isRequired="true"
@@ -107,6 +163,33 @@
               v-bind="selectedSchuleProps"
               v-model="selectedSchule"
               v-model:search="searchInputSchule"
+            ></v-autocomplete>
+          </FormRow>
+
+          <!-- Rollenauswahl -->
+          <FormRow
+            :errorLabel="selectedRolleProps['error']"
+            labelForId="rolle-select"
+            :isRequired="true"
+            :label="$t('admin.rolle.rolle')"
+          >
+            <v-autocomplete
+              autocomplete="off"
+              clearable
+              @clear="clearSelectedRolle"
+              data-testid="rolle-select"
+              density="compact"
+              id="rolle-select"
+              ref="rolle-select"
+              :items="rollen"
+              item-value="value"
+              item-text="title"
+              :no-data-text="$t('noDataFound')"
+              :placeholder="$t('admin.rolle.selectRolle')"
+              required="true"
+              variant="outlined"
+              v-bind="selectedRolleProps"
+              v-model="selectedRolle"
             ></v-autocomplete>
           </FormRow>
         </v-container>
