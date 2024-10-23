b61768c45ae47d98badb77ae71868ad28588f5d9
Henrik Meyer <henrik.meyer@capgemini.com>
2024-10-23 11:34:06 +0200

    Add cspNonce variable to test

--- src/components/admin/personen/PasswordReset.spec.ts
+++ src/components/admin/personen/PasswordReset.spec.ts
@@ -5,7 +5,13 @@ import PasswordReset from './PasswordReset.vue';
 
 let wrapper: VueWrapper | null = null;
 
+declare global {
+  // eslint-disable-next-line no-var
+  var cspNonce: string;
+}
+
 beforeEach(() => {
+  globalThis.cspNonce = 'CSPN0NCEPLAC3H0LDER';
   document.body.innerHTML = `
     <div>
       <div id="app"></div>
@@ -86,25 +92,25 @@ describe('reset password', () => {
     expect(document.querySelector('[data-testid="password-output-field"] mdi-content-copy')).not.toBeNull();
   });
 
-  // it('should open the print dialog if print password button was clicked', async () => {
-  //   const mockWindow: Partial<Window> = {
-  //     document: {
-  //       open: vi.fn(function (this: void) {}),
-  //       write: vi.fn(function (this: void) {}),
-  //       close: vi.fn(function (this: void) {}),
-  //     },
-  //     print: vi.fn(function (this: void) {}),
-  //   } as unknown as Window;
-  //   window.open = vi.fn(() => mockWindow) as unknown as typeof window.open;
+  it('should open the print dialog if print password button was clicked', async () => {
+    const mockWindow: Partial<Window> = {
+      document: {
+        open: vi.fn(function (this: void) {}),
+        write: vi.fn(function (this: void) {}),
+        close: vi.fn(function (this: void) {}),
+      },
+      print: vi.fn(function (this: void) {}),
+    } as unknown as Window;
+    window.open = vi.fn(() => mockWindow) as unknown as typeof window.open;
 
-  //   await wrapper?.find('[data-testid="open-password-reset-dialog-icon"]').trigger('click');
-  //   const passwordPrintButton: Element | null = await document.querySelector('[data-testid="password-print-button"]');
-  //   if (passwordPrintButton instanceof HTMLButtonElement) {
-  //     passwordPrintButton.click();
-  //   }
+    await wrapper?.find('[data-testid="open-password-reset-dialog-icon"]').trigger('click');
+    const passwordPrintButton: Element | null = await document.querySelector('[data-testid="password-print-button"]');
+    if (passwordPrintButton instanceof HTMLButtonElement) {
+      passwordPrintButton.click();
+    }
 
-  //   expect(mockWindow.print).toHaveBeenCalled();
-  // });
+    expect(mockWindow.print).toHaveBeenCalled();
+  });
 });
 
 afterEach(() => {
