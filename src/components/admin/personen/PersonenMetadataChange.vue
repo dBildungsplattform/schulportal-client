c2ae2b036a5933aab04f4759e40020ba2d5ea31e
Alexander Ungefug <82446024+AlexanderUngefug@users.noreply.github.com>
2024-10-25 14:25:58 +0200

    text message style

--- src/components/admin/personen/PersonenMetadataChange.vue
+++ src/components/admin/personen/PersonenMetadataChange.vue
@@ -74,34 +74,41 @@
   <div>
     <v-row>
       <v-col
-      md="5"
-  xs="12"
-  cols="12"
-  class="pl-md-14"
+        md="5"
+        xs="12"
+        cols="12"
+        class="pl-md-14"
       >
         <span class="text-body bold">
-          <v-icon
-            aria-hidden="true"
-            class="mr-2"
-            icon="mdi-alert-circle-outline"
-            size="small"
-          ></v-icon>
-          <span> {{ $t('admin.person.personalInfoChangeNotice') }} </span>
-          <span>
-            <a
-              :href="link.href"
-              :target="link.external ? '_blank' : '_self'"
-            >
-              {{ link.text }}</a
-            >.
-          </span>
+          <div>
+            <v-icon
+              aria-hidden="true"
+              class="mr-2"
+              icon="mdi-alert-circle-outline"
+              size="small"
+            ></v-icon>
+            <span> {{ $t('admin.person.personalInfoChangeNotice') }} </span>
+          </div>
+          <div class="mt-4">
+            <p>
+              {{ $t('admin.person.personalInfoChangeNotice2') }}
+              <span>
+                <a
+                  :href="link.href"
+                  :target="link.external ? '_blank' : '_self'"
+                >
+                  {{ link.text }}</a
+                >.
+              </span>
+            </p>
+          </div>
         </span>
       </v-col>
       <v-col
-      md="7"
-  xs="12"
-  cols="12"
-  class="pr-md-14"
+        md="7"
+        xs="12"
+        cols="12"
+        class="pr-md-14"
       >
         <!-- Vorname -->
         <FormRow
