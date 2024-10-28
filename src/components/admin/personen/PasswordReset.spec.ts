4c12b96e13d2e67a7459d2bd2d30a2dde5a3aab4
Henrik Meyer <151522698+he-meyer@users.noreply.github.com>
2024-10-28 11:52:56 +0100

    SPSH-1111 (#286)
    * Add keys to de-DE locale
    
    * Add SPSH tooltips to password output component
    
    * Implement password printing and adapt buttons in PasswordReset
    
    * Add tests
    
    * Change labels and tooltips
    
    * Add px
    
    * Move CSS styling for printing to separate file
    
    * Use nonce to allow CSS styling for print
    
    * Use string
    
    * Use nonce variable
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Remove debug statements
    
    * Add cspNonce variable to test
    
    * Add html doctype and lang
    
    * Move cspNonce variable to global.d.ts
    
    * Remove unnecessary line
    
    ---------
    
    Co-authored-by: Youssef Bouchara <101522419+YoussefBouch@users.noreply.github.com>

--- src/components/admin/personen/PasswordReset.spec.ts
+++ src/components/admin/personen/PasswordReset.spec.ts
@@ -6,7 +6,13 @@ import { EmailAddressStatus } from '@/api-client/generated/api';
 
 let wrapper: VueWrapper | null = null;
 
+declare global {
+  // eslint-disable-next-line no-var
+  var cspNonce: string;
+}
+
 beforeEach(() => {
+  globalThis.cspNonce = 'CSPN0NCEPLAC3H0LDER';
   document.body.innerHTML = `
     <div>
       <div id="app"></div>
@@ -72,7 +78,8 @@ describe('reset password', () => {
   // Using the document's querySelector works to find elements, so we can test if they exist.
   // But I haven't found a way to trigger events with the querySelector and emit them to the wrapper to assert them.
 
-  test.skip('reset button emits correct event', async () => {
+  test('reset button emits correct event', async () => {
+    await wrapper?.setProps({ password: '' });
     wrapper?.get('[data-testid="open-password-reset-dialog-icon"]').trigger('click');
     await nextTick();
     await document.querySelector('[data-testid="password-reset-button"]');
@@ -84,6 +91,12 @@ describe('reset password', () => {
 
     // console.log(dialog?.emitted());
     // expect(dialog?.emitted('onResetPassword')).toBeTruthy();
+  });
+
+  it('should render the print button if password was reset', async () => {
+    wrapper?.get('[data-testid="open-password-reset-dialog-icon"]').trigger('click');
+    await document.querySelector('[data-testid="password-print-button"]');
+    expect(document.querySelector('[data-testid="password-print-button"]')).not.toBeNull();
   });
 
   // skip because v-dialog does not work in test env. see lines 42-47
@@ -98,6 +111,26 @@ describe('reset password', () => {
     wrapper?.get('[data-testid="open-password-reset-dialog-icon"]').trigger('click');
     await document.querySelector('[data-testid="password-output-field"] mdi-content-copy');
     expect(document.querySelector('[data-testid="password-output-field"] mdi-content-copy')).not.toBeNull();
+  });
+
+  it('should open the print dialog if print password button was clicked', async () => {
+    const mockWindow: Partial<Window> = {
+      document: {
+        open: vi.fn(function (this: void) {}),
+        write: vi.fn(function (this: void) {}),
+        close: vi.fn(function (this: void) {}),
+      },
+      print: vi.fn(function (this: void) {}),
+    } as unknown as Window;
+    window.open = vi.fn(() => mockWindow) as unknown as typeof window.open;
+
+    await wrapper?.find('[data-testid="open-password-reset-dialog-icon"]').trigger('click');
+    const passwordPrintButton: Element | null = await document.querySelector('[data-testid="password-print-button"]');
+    if (passwordPrintButton instanceof HTMLButtonElement) {
+      passwordPrintButton.click();
+    }
+
+    expect(mockWindow.print).toHaveBeenCalled();
   });
 });
 
