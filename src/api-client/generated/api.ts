7470600c67500b8f3bdd6de2d2c5fd6d12cad5d0
godtierbatuhan <boucharay1001@gmail.com>
2024-11-05 17:57:48 +0100

    spec

--- src/api-client/generated/api.ts
+++ src/api-client/generated/api.ts
@@ -2437,7 +2437,6 @@ export const RollenSystemRecht = {
     MigrationDurchfuehren: 'MIGRATION_DURCHFUEHREN',
     PersonSynchronisieren: 'PERSON_SYNCHRONISIEREN',
     CronDurchfuehren: 'CRON_DURCHFUEHREN',
-    PersonenAnlegen: 'PERSONEN_ANLEGEN',
     ImportDurchfuehren: 'IMPORT_DURCHFUEHREN'
 } as const;
 
