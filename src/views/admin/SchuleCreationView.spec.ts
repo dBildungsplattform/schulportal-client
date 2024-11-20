09e3316f31313a2d0249c83aca59b08dc0f42ebd
Timo K <timo.kleist@capgemini.com>
2024-11-20 15:24:49 +0100

    add schulecreationview test

--- src/views/admin/SchuleCreationView.spec.ts
+++ src/views/admin/SchuleCreationView.spec.ts
@@ -123,9 +123,19 @@ describe('SchuleCreationView', () => {
     expect(wrapper?.find('[data-testid="alert-title"]').isVisible()).toBe(true);
   });
 
-  test('shows error message if REQUIRED_STEP_UP_LEVEL_NOT_MET error is present', async () => {
+  test('shows error message if REQUIRED_STEP_UP_LEVEL_NOT_MET error is present and click close button', async () => {
     organisationStore.errorCode = 'REQUIRED_STEP_UP_LEVEL_NOT_MET';
     await nextTick();
     expect(wrapper?.find('[data-testid="alert-title"]').isVisible()).toBe(true);
+    wrapper?.find('[data-testid="alert-button"]').trigger('click');
+    await nextTick();
+  });
+
+  test('shows error message if REQUIRED_STEP_UP_LEVEL_NOT_MET error is present and click x button', async () => {
+    organisationStore.errorCode = 'REQUIRED_STEP_UP_LEVEL_NOT_MET';
+    await nextTick();
+    expect(wrapper?.find('[data-testid="alert-title"]').isVisible()).toBe(true);
+    wrapper?.find('[data-testid="close-layout-card-button"]').trigger('click');
+    await nextTick();
   });
 });
