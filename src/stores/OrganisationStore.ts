7a0b3514cd4069459b5738e204ee71eaa439a5c8
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2024-11-21 12:18:14 +0100

    use different endpoint to get all classes of a school

--- src/stores/OrganisationStore.ts
+++ src/stores/OrganisationStore.ts
@@ -331,13 +331,17 @@ export const useOrganisationStore: StoreDefinition<
       this.errorCode = '';
       this.loadingKlassen = true;
       try {
-        const response: AxiosResponse<Organisation[]> =
-          await organisationApi.organisationControllerGetAdministrierteOrganisationen(
-            organisationId,
-            filter?.offset,
-            filter?.limit,
-            filter?.searchString,
-          );
+        const response: AxiosResponse<Organisation[]> = await organisationApi.organisationControllerFindOrganizations(
+          filter?.offset,
+          filter?.limit,
+          undefined,
+          undefined,
+          filter?.searchString,
+          OrganisationsTyp.Klasse,
+          [],
+          undefined,
+          [organisationId],
+        );
         const getFilteredKlassen: Organisation[] = response.data.filter(
           (orga: Organisation) => orga.typ === OrganisationsTyp.Klasse,
         );
