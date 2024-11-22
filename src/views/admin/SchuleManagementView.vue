9fc1edcad94bc379252f7c8c389c8181f4c33bf7
Youssef Bouchara <101522419+YoussefBouch@users.noreply.github.com>
2024-11-22 09:24:10 +0100

    SPSH-1342 (#332)
    * Implemented button/icon to activate the Schule in Itslearning
    
    * Tests
    
    * Tests
    
    * fixed build and lint
    
    * run lint
    
    * PO Review.
    
    * fixed text
    
    * more left margin
    
    * Fixed error handling
    
    * Lint
    
    * PO comment
    
    * fixed local

--- src/views/admin/SchuleManagementView.vue
+++ src/views/admin/SchuleManagementView.vue
@@ -6,9 +6,14 @@
   import type { VDataTableServer } from 'vuetify/lib/components/index.mjs';
   import { OrganisationsTyp, useOrganisationStore, type OrganisationStore } from '@/stores/OrganisationStore';
   import { type SearchFilterStore, useSearchFilterStore } from '@/stores/SearchFilterStore';
+  import ItsLearningSetup from '@/components/admin/schulen/itsLearningSetup.vue';
+  import SpshAlert from '@/components/alert/SpshAlert.vue';
+  import { onBeforeRouteLeave, useRouter, type Router } from 'vue-router';
 
   const organisationStore: OrganisationStore = useOrganisationStore();
   const searchFilterStore: SearchFilterStore = useSearchFilterStore();
+
+  const router: Router = useRouter();
 
   const { t }: Composer = useI18n({ useScope: 'global' });
 
@@ -20,6 +25,7 @@
       align: 'start',
     },
     { title: t('admin.schule.schulname'), key: 'name', align: 'start' },
+    { title: t('admin.schule.itsLearningStatus'), key: 'itslearning', sortable: false, align: 'start' },
   ];
 
   function getPaginatedSchulen(page: number): void {
@@ -47,6 +53,15 @@
     });
   }
 
+  async function toggleItsLearningStatus(organisationId: string): Promise<void> {
+    await organisationStore.setItsLearningForSchule(organisationId);
+  }
+
+  const handleAlertClose = (): void => {
+    organisationStore.errorCode = '';
+    router.go(0);
+  };
+
   onMounted(async () => {
     await organisationStore.getAllOrganisationen({
       offset: (searchFilterStore.schulenPage - 1) * searchFilterStore.schulenPerPage,
@@ -54,6 +69,10 @@
       includeTyp: OrganisationsTyp.Schule,
       systemrechte: ['SCHULEN_VERWALTEN'],
     });
+  });
+
+  onBeforeRouteLeave(async () => {
+    organisationStore.errorCode = '';
   });
 </script>
 
@@ -66,29 +85,60 @@
       {{ $t('admin.headline') }}
     </h1>
     <LayoutCard :header="$t('admin.schule.management')">
-      <ResultTable
-        :currentPage="searchFilterStore.schulenPage"
-        data-testid="schule-table"
-        :items="organisationStore.allSchulen || []"
-        :loading="organisationStore.loading"
-        :headers="headers"
-        item-value-path="id"
-        :disableRowClick="true"
-        @onItemsPerPageUpdate="getPaginatedSchulenWithLimit"
-        @onPageUpdate="getPaginatedSchulen"
-        ref="result-table"
-        :totalItems="organisationStore.totalSchulen"
-        :itemsPerPage="searchFilterStore.schulenPerPage"
-      >
-        <template v-slot:[`item.name`]="{ item }">
-          <div
-            class="ellipsis-wrapper"
-            :title="item.name"
-          >
-            {{ item.name }}
-          </div>
-        </template></ResultTable
-      >
+      <!-- Error Message Display -->
+      <SpshAlert
+        :model-value="!!organisationStore.errorCode"
+        :title="
+          organisationStore.errorCode === 'UNSPECIFIED_ERROR'
+            ? $t('admin.schule.loadingErrorTitle')
+            : $t(`admin.schule.title.${organisationStore.errorCode}`)
+        "
+        :type="'error'"
+        :closable="false"
+        :text="
+          organisationStore.errorCode === 'UNSPECIFIED_ERROR'
+            ? $t('admin.schule.loadingErrorText')
+            : $t(`admin.schule.errors.${organisationStore.errorCode}`)
+        "
+        :showButton="true"
+        :buttonText="$t('nav.backToList')"
+        :buttonAction="handleAlertClose"
+        @update:modelValue="handleAlertClose"
+      />
+      <template v-if="!organisationStore.errorCode">
+        <ResultTable
+          :currentPage="searchFilterStore.schulenPage"
+          data-testid="schule-table"
+          :items="organisationStore.allSchulen || []"
+          :loading="organisationStore.loading"
+          :headers="headers"
+          item-value-path="id"
+          :disableRowClick="true"
+          @onItemsPerPageUpdate="getPaginatedSchulenWithLimit"
+          @onPageUpdate="getPaginatedSchulen"
+          ref="result-table"
+          :totalItems="organisationStore.totalSchulen"
+          :itemsPerPage="searchFilterStore.schulenPerPage"
+        >
+          <template v-slot:[`item.name`]="{ item }">
+            <div
+              class="ellipsis-wrapper"
+              :title="item.name"
+            >
+              {{ item.name }}
+            </div>
+          </template>
+          <template v-slot:[`item.itslearning`]="{ item }">
+            <ItsLearningSetup
+              :errorCode="organisationStore.errorCode"
+              :schulname="item.name"
+              :schulId="item.id"
+              :itslearningEnabled="item.itslearningEnabled"
+              @onActivateItslearning="toggleItsLearningStatus(item.id)"
+            ></ItsLearningSetup>
+          </template>
+        </ResultTable>
+      </template>
     </LayoutCard>
   </div>
 </template>
