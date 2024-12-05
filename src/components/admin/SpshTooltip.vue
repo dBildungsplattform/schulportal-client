70b2a6427c3a7a617dfaf29ba26491aefce0c2f1
Timo K <timo.kleist@capgemini.com>
2024-12-05 16:19:21 +0100

    tryout mobile hover fix

--- src/components/admin/SpshTooltip.vue
+++ src/components/admin/SpshTooltip.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+  import { ref, onMounted, onBeforeUnmount, type Ref } from 'vue';
+
+  // Props definition
   defineProps({
     enabledCondition: {
       type: Boolean,
@@ -14,13 +17,41 @@
       default: 'top',
     },
   });
+
+  // Reactive state for tooltip visibility
+  const tooltipVisible: Ref<boolean> = ref(false);
+
+  // Determine if the user is on a mobile device
+  const isMobile: Ref<boolean> = ref(false);
+
+  onMounted(() => {
+    isMobile.value = /Mobi|Android/i.test(navigator.userAgent);
+  });
+
+  // Close tooltip on external touch
+  const closeTooltip = (event: Event): void => {
+    if (!(event.target as Element).closest('[data-testid="tooltip"]')) {
+      tooltipVisible.value = false;
+    }
+  };
+
+  onMounted(() => {
+    document.addEventListener('touchstart', closeTooltip);
+  });
+
+  onBeforeUnmount(() => {
+    document.removeEventListener('touchstart', closeTooltip);
+  });
 </script>
 
 <template>
   <v-tooltip
+    v-model="tooltipVisible"
     data-testid="tooltip"
     :location="position"
     open-delay="500"
+    :open-on-hover="!isMobile"
+    :open-on-click="isMobile"
   >
     <template v-slot:activator="{ props: tooltipProps }">
       <div v-bind="tooltipProps">
@@ -30,5 +61,3 @@
     <span data-testid="tooltip-text">{{ enabledCondition ? enabledText : disabledText }}</span>
   </v-tooltip>
 </template>
-
-<style></style>
