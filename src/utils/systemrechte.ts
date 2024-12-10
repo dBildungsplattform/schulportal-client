231a59c6e784fbc2c87a5100923a983a6a358b35
Timo K <timo.kleist@capgemini.com>
2024-12-10 13:24:02 +0100

    rename function

--- src/utils/systemrechte.ts
+++ src/utils/systemrechte.ts
@@ -1,6 +1,6 @@
 import { RollenSystemRecht } from '@/api-client/generated';
 
-export function hideSystemrecht(enumValue: RollenSystemRecht): boolean {
+export function isHiddenSystemrecht(enumValue: RollenSystemRecht): boolean {
   return (
     enumValue == RollenSystemRecht.MigrationDurchfuehren ||
     enumValue == RollenSystemRecht.CronDurchfuehren ||
