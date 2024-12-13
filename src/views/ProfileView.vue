85e26ef4f1bbbffe364fd61adc2f6fcc0d9a8d9b
DeFi3298 <dennis.fischer@capgemini.com>
2024-12-13 17:30:56 +0100

    Visibility Fix

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -738,6 +738,7 @@
                 class="pt-4 text-center text-body-1 text-medium-emphasis"
                 data-testid="schuladmins-info-text-with-icon"
                 style="white-space: normal"
+                v-if="schuleData.schulAdmins.length > 0"
               >
                 <v-icon
                   class="mr-2"
