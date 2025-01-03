b4d36a6bc70e436c45d4cb6ecd3bd6ac1826fa18
godtierbatuhan <boucharay1001@gmail.com>
2025-01-03 11:06:36 +0100

    Sorting by name is always there by default

--- src/views/admin/PersonManagementView.vue
+++ src/views/admin/PersonManagementView.vue
@@ -66,8 +66,6 @@
       !!searchFilterStore.selectedOrganisationen?.length ||
       !!searchFilterStore.selectedRollen?.length ||
       !!searchFilterStore.searchFilterPersonen ||
-      !!searchFilterStore.sortField ||
-      !!searchFilterStore.sortOrder ||
       selectedKlassen.value.length > 0 ||
       !!selectedStatus.value,
   );
@@ -234,8 +232,6 @@
     selectedStatus.value = null;
     searchFilterStore.personenPage = 1;
     searchFilterStore.personenPerPage = 30;
-    searchFilterStore.sortField = '';
-    searchFilterStore.sortOrder = '';
     searchFilterStore.currentSort = null;
     personStore.getAllPersons({
       offset: (searchFilterStore.personenPage - 1) * searchFilterStore.personenPerPage,
