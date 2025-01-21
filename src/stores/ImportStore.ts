d51b37ae6e771e6abedc4d127c13c55636ddda20
godtierbatuhan <boucharay1001@gmail.com>
2025-01-22 00:04:57 +0100

    Test with 300

--- src/stores/ImportStore.ts
+++ src/stores/ImportStore.ts
@@ -63,7 +63,7 @@ export const useImportStore: StoreDefinition<'importStore', ImportState, ImportG
       uploadResponse: null,
       importStatus: null,
       importProgress: 0,
-      importedUsersPerPage: 400,
+      importedUsersPerPage: 300,
       importedUsersPage: 1,
     };
   },
