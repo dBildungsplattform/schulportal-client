0bdba4a8316e5d134a1c808007a0ad41774f0d0c
godtierbatuhan <boucharay1001@gmail.com>
2025-02-12 20:55:26 +0100

    Test for the new vue

--- src/views/admin/SchultraegerCreationView.spec.ts
+++ src/views/admin/SchultraegerCreationView.spec.ts
@@ -1,21 +1,58 @@
-import { expect, test, type MockInstance } from 'vitest';
+import { expect, test, type Mock, type MockInstance } from 'vitest';
 import { VueWrapper, flushPromises, mount } from '@vue/test-utils';
-import SchuleCreationView from './SchuleCreationView.vue';
-import { createRouter, createWebHistory, type Router } from 'vue-router';
+import {
+  createRouter,
+  createWebHistory,
+  type NavigationGuardNext,
+  type RouteLocationNormalized,
+  type Router,
+} from 'vue-router';
 import routes from '@/router/routes';
 import { nextTick } from 'vue';
 import { OrganisationsTyp, useOrganisationStore, type OrganisationStore } from '@/stores/OrganisationStore';
 import type { OrganisationResponse } from '@/api-client/generated';
 import SchultraegerCreationView from './SchultraegerCreationView.vue';
+import type Module from 'module';
 
 let wrapper: VueWrapper | null = null;
 let router: Router;
 let organisationStore: OrganisationStore;
 
+type OnBeforeRouteLeaveCallback = (
+  _to: RouteLocationNormalized,
+  _from: RouteLocationNormalized,
+  _next: NavigationGuardNext,
+) => void;
+let { storedBeforeRouteLeaveCallback }: { storedBeforeRouteLeaveCallback: OnBeforeRouteLeaveCallback } = vi.hoisted(
+  () => {
+    return {
+      storedBeforeRouteLeaveCallback: (
+        _to: RouteLocationNormalized,
+        _from: RouteLocationNormalized,
+        _next: NavigationGuardNext,
+      ): void => {},
+    };
+  },
+);
+
+function mountComponent(): VueWrapper {
+  return mount(SchultraegerCreationView, {
+    attachTo: document.getElementById('app') || '',
+    global: {
+      components: {
+        SchultraegerCreationView,
+      },
+      plugins: [router],
+    },
+  });
+}
+
 beforeEach(async () => {
   document.body.innerHTML = `
     <div>
-      <div id="app"></div>
+        <router-view>
+            <div id="app"></div>
+         </router-view>
     </div>
   `;
 
@@ -50,20 +87,11 @@ beforeEach(async () => {
   router.push('/');
   await router.isReady();
 
-  wrapper = mount(SchultraegerCreationView, {
-    attachTo: document.getElementById('app') || '',
-    global: {
-      components: {
-        SchuleCreationView,
-      },
-      mocks: {
-        route: {
-          fullPath: 'full/path',
-        },
-      },
-      plugins: [router],
-    },
-  });
+  wrapper = mountComponent();
+});
+
+afterEach(() => {
+  wrapper?.unmount();
 });
 
 describe('SchultraegerView', () => {
@@ -125,5 +153,47 @@ describe('SchultraegerView', () => {
     expect(wrapper?.find('[data-testid="alert-title"]').isVisible()).toBe(true);
     wrapper?.find('[data-testid="alert-button"]').trigger('click');
     await nextTick();
+  });
+
+  describe('navigation interception', () => {
+    afterEach(() => {
+      vi.unmock('vue-router');
+    });
+
+    test('triggers unsaved changes dialog when form is dirty', async () => {
+      const expectedCallsToNext: number = 0;
+      // Mock onBeforeRouteLeave to capture the callback
+      vi.mock('vue-router', async (importOriginal: () => Promise<Module>) => {
+        const mod: Module = await importOriginal();
+        return {
+          ...mod,
+          onBeforeRouteLeave: vi.fn((actualCallback: OnBeforeRouteLeaveCallback) => {
+            storedBeforeRouteLeaveCallback = actualCallback;
+          }),
+        };
+      });
+
+      // Remount the component to make sure the form is empty at first
+      wrapper = mountComponent();
+      await nextTick();
+
+      // Fill the form to make it dirty
+      const schulnameInput: VueWrapper | undefined = wrapper.findComponent({ ref: 'schultraegername-input' });
+      await schulnameInput.setValue('Random Schultraegername');
+      await nextTick();
+
+      const spy: Mock = vi.fn();
+      storedBeforeRouteLeaveCallback({} as RouteLocationNormalized, {} as RouteLocationNormalized, spy);
+      expect(spy).toHaveBeenCalledTimes(expectedCallsToNext);
+      await nextTick();
+
+      // Simulate user confirming the navigation
+      const confirmButton: Element | null = document.querySelector('[data-testid="confirm-unsaved-changes-button"]');
+      expect(confirmButton).not.toBeNull();
+      confirmButton?.dispatchEvent(new Event('click'));
+      await nextTick();
+
+      expect(spy).toHaveBeenCalledOnce();
+    });
   });
 });
