c607aae434014bddd8162f29eb343afeb14dcd30
godtierbatuhan <boucharay1001@gmail.com>
2025-03-14 23:05:15 +0100

    Coverage to the minimum

--- src/views/admin/organisationen/SchultraegerDetailsView.spec.ts
+++ src/views/admin/organisationen/SchultraegerDetailsView.spec.ts
@@ -1,14 +1,63 @@
-import { expect, test } from 'vitest';
+import { expect, test, type Mock, type MockInstance } from 'vitest';
 import { VueWrapper, mount } from '@vue/test-utils';
-import { createRouter, createWebHistory, type Router } from 'vue-router';
+import {
+  createRouter,
+  createWebHistory,
+  type NavigationGuardNext,
+  type RouteLocationNormalized,
+  type Router,
+} from 'vue-router';
 import routes from '@/router/routes';
 import { nextTick } from 'vue';
 import { OrganisationsTyp, useOrganisationStore, type OrganisationStore } from '@/stores/OrganisationStore';
 import SchultraegerDetailsView from './SchultraegerDetailsView.vue';
+import type Module from 'module';
 
 let wrapper: VueWrapper | null = null;
 let router: Router;
 let organisationStore: OrganisationStore;
+
+type OnBeforeRouteLeaveCallback = (
+  _to: RouteLocationNormalized,
+  _from: RouteLocationNormalized,
+  _next: NavigationGuardNext,
+) => void;
+
+let { storedBeforeRouteLeaveCallback }: { storedBeforeRouteLeaveCallback: OnBeforeRouteLeaveCallback } = vi.hoisted(
+  () => {
+    return {
+      storedBeforeRouteLeaveCallback: (
+        _to: RouteLocationNormalized,
+        _from: RouteLocationNormalized,
+        _next: NavigationGuardNext,
+      ): void => {},
+    };
+  },
+);
+
+type FormFields = {
+  schultraegerName: string;
+};
+
+type FormSelectors = {
+  schultraegerNameInput: VueWrapper;
+};
+
+async function fillForm(args: Partial<FormFields>): Promise<Partial<FormSelectors>> {
+  const { schultraegerName }: Partial<FormFields> = args;
+  const selectors: Partial<FormSelectors> = {};
+
+  const schultraegerNameInput: VueWrapper | undefined = wrapper
+    ?.findComponent({ ref: 'schultraeger-edit-form' })
+    .findComponent({ ref: 'schultraegername-input' });
+  expect(schultraegerNameInput?.exists()).toBe(true);
+
+  await schultraegerNameInput?.setValue(schultraegerName);
+  await nextTick();
+  selectors.schultraegerNameInput = schultraegerNameInput;
+
+  return selectors;
+}
 
 function mountComponent(): VueWrapper {
   return mount(SchultraegerDetailsView, {
@@ -177,5 +226,95 @@ describe('SchultraegerDetailsView', () => {
 
     organisationStore.updatedOrganisation = null;
     await nextTick();
+  });
+
+  test('it navigates back to Schultraeger management', async () => {
+    const push: MockInstance = vi.spyOn(router, 'push');
+    await wrapper?.find('[data-testid="close-layout-card-button"]').trigger('click');
+    expect(push).toHaveBeenCalledTimes(1);
+    expect(wrapper?.findComponent({ ref: 'result-table' }).exists());
+  });
+
+  test('it navigates back to Schultraeger details', async () => {
+    organisationStore.errorCode = 'MOCK_ERROR';
+    await nextTick();
+
+    const push: MockInstance = vi.spyOn(router, 'push');
+    await wrapper?.find('[data-testid="alert-button"]').trigger('click');
+    await nextTick();
+
+    expect(push).toHaveBeenCalledTimes(1);
+  });
+
+  
+
+  describe('navigation interception', () => {
+    afterEach(() => {
+      vi.unmock('vue-router');
+    });
+
+    test('it triggers if form is dirty', async () => {
+      const expectedCallsToNext: number = 0;
+      vi.mock('vue-router', async (importOriginal: () => Promise<Module>) => {
+        const mod: Module = await importOriginal();
+        return {
+          ...mod,
+          onBeforeRouteLeave: vi.fn((actualCallback: OnBeforeRouteLeaveCallback) => {
+            storedBeforeRouteLeaveCallback = actualCallback;
+          }),
+        };
+      });
+
+      wrapper = mountComponent();
+      await fillForm({
+        schultraegerName: 'Traeger 1',
+      });
+      await nextTick();
+
+      const spy: Mock = vi.fn();
+      storedBeforeRouteLeaveCallback({} as RouteLocationNormalized, {} as RouteLocationNormalized, spy);
+      expect(spy).toHaveBeenCalledTimes(expectedCallsToNext);
+      await nextTick();
+
+      const confirmButton: Element | null = document.querySelector('[data-testid="confirm-unsaved-changes-button"]');
+      expect(confirmButton).not.toBeNull();
+      confirmButton!.dispatchEvent(new Event('click'));
+      expect(spy).toHaveBeenCalledOnce();
+    });
+
+    test('it does not trigger if form is not dirty', async () => {
+      const expectedCallsToNext: number = 1;
+      vi.mock('vue-router', async (importOriginal: () => Promise<Module>) => {
+        const mod: Module = await importOriginal();
+        return {
+          ...mod,
+          onBeforeRouteLeave: vi.fn((actualCallback: OnBeforeRouteLeaveCallback) => {
+            storedBeforeRouteLeaveCallback = actualCallback;
+          }),
+        };
+      });
+      wrapper = mountComponent();
+      const spy: Mock = vi.fn();
+      storedBeforeRouteLeaveCallback({} as RouteLocationNormalized, {} as RouteLocationNormalized, spy);
+      expect(spy).toHaveBeenCalledTimes(expectedCallsToNext);
+    });
+  });
+
+  describe.each([[true], [false]])('when form is dirty:%s', async (isFormDirty: boolean) => {
+    beforeEach(async () => {
+      await fillForm({
+        schultraegerName: 'Traeger 1',
+      });
+      await nextTick();
+    });
+
+    test('it handles unloading', async () => {
+      const event: Event = new Event('beforeunload');
+      const spy: MockInstance = vi.spyOn(event, 'preventDefault');
+      window.dispatchEvent(event);
+
+      if (isFormDirty) expect(spy).toHaveBeenCalled();
+      else expect(spy).not.toHaveBeenCalledOnce();
+    });
   });
 });
