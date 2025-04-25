d73029410a957902bb886626a0126390187a840f
Yannick Clausen <yannick.clausen@capgemini.com>
2025-04-25 15:39:32 +0200

    keep old behavior in errorHandler

--- src/utils/errorHandlers.ts
+++ src/utils/errorHandlers.ts
@@ -3,9 +3,9 @@ import { isAxiosError } from 'axios';
 export function getResponseErrorCode(error: unknown, defaultErrorCode: string): string {
   if (isAxiosError(error)) {
     /* some endpoints respond an error string inside data.i18nKey, some endpoints respond with an error string inside data.code */
-    const errorCode: string | undefined = error.response?.data?.i18nKey ?? error.response?.data?.code;
+    const errorCode: string = error.response?.data.i18nKey ? error.response.data.i18nKey : error.response?.data.code;
 
-    return errorCode ?? defaultErrorCode;
+    return errorCode || defaultErrorCode;
   }
 
   /* if an unknown error occurs, return UNSPECIFIED_ERROR */
@@ -18,7 +18,7 @@ export function getResponseErrorMesage(error: unknown): string {
   let errorCode: string = 'UNSPECIFIED_ERROR';
 
   if (isAxiosError(error)) {
-    errorCode = error.response?.data?.message ?? 'UNSPECIFIED_ERROR';
+    errorCode = error.response?.data.message ? error.response.data.message : 'UNSPECIFIED_ERROR';
   }
 
   return errorCode;
