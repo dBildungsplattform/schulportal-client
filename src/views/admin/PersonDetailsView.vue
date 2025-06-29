09b94706697944053ab24d9cbf10be7e772560b3
godtierbatuhan <boucharay1001@gmail.com>
2025-06-29 13:44:13 +0200

    Fixed build

--- src/views/admin/PersonDetailsView.vue
+++ src/views/admin/PersonDetailsView.vue
@@ -1500,7 +1500,7 @@
     const twoFARequirementPromise: Promise<void> = twoFactorAuthentificationStore.get2FARequirement(currentPersonId);
     const personByIdPromise: Promise<Personendatensatz> = personStore.getPersonById(currentPersonId);
     const personUebersichtPromise: Promise<void> = personStore.getPersonenuebersichtById(currentPersonId);
-    const workflowStepPromise: Promise<PersonenkontextWorkflowResponse> = personenkontextStore.processWorkflowStep({
+    const workflowStepPromise: Promise<void> = personenkontextStore.processWorkflowStep({
       personId: currentPersonId,
       operationContext: OperationContext.PERSON_BEARBEITEN,
       limit: 25,
