0ed3c12f6482eb3a0c98afad23509051d5cc67ae
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2025-09-09 10:04:01 +0200

    change Systemrecht from class to type

--- src/stores/MasterDataStore.ts
+++ src/stores/MasterDataStore.ts
@@ -1,19 +1,18 @@
 import { defineStore, type Store, type StoreDefinition } from 'pinia';
 import { RolleApiFactory, type RolleApiInterface, type SystemRechtResponse } from '../api-client/generated/api';
 import axiosApiInstance from '@/services/ApiService';
-import { SystemRecht } from './types/Systemrecht';
+import { type SystemRechtType } from './types/Systemrecht';
 
 const rolleApi: RolleApiInterface = RolleApiFactory(undefined, '', axiosApiInstance);
 
 export enum MasterDataStatus {
   Uninitialized = 'UNINITIALIZED',
   Initialized = 'INITIALIZED',
-  Pending = 'PENDING',
   Failed = 'FAILED',
 }
 
 export type MasterDataState = {
-  systemrechte: SystemRecht[];
+  systemrechte: SystemRechtType[];
   status: MasterDataStatus;
 };
 
@@ -43,7 +42,7 @@ export const useMasterDataStore: StoreDefinition<
     initialise: async function (): Promise<void> {
       try {
         const { data }: { data: SystemRechtResponse[] } = await rolleApi.rolleControllerGetAllSystemrechte();
-        this.systemrechte = data.map((sr: SystemRechtResponse) => new SystemRecht(sr.name, sr.isTechnical));
+        this.systemrechte = data;
         this.status = MasterDataStatus.Initialized;
       } catch (error) {
         this.status = MasterDataStatus.Failed;
@@ -53,7 +52,8 @@ export const useMasterDataStore: StoreDefinition<
       return this.status === MasterDataStatus.Initialized;
     },
     isHiddenSystemrecht: function (enumValue: string): boolean {
-      return this.systemrechte.find((sr) => sr.name === enumValue)?.isTechnical || false;
+      const systemrechteTmp: SystemRechtType[] = this.systemrechte;
+      return systemrechteTmp.find((sr: SystemRechtType) => sr.name === enumValue)?.isTechnical || false;
     },
   },
 });
--- src/stores/types/Systemrecht.ts
+++ src/stores/types/Systemrecht.ts
@@ -1,20 +1,6 @@
 import type { RollenSystemRechtEnum } from '@/api-client/generated/api';
 
-export class SystemRecht {
-  private readonly nameInternal: RollenSystemRechtEnum;
-
-  private readonly technical: boolean;
-
-  public constructor(name: RollenSystemRechtEnum, technical: boolean) {
-    this.nameInternal = name;
-    this.technical = technical;
-  }
-
-  public get name(): RollenSystemRechtEnum {
-    return this.nameInternal;
-  }
-
-  public get isTechnical(): boolean {
-    return this.technical;
-  }
-}
+export type SystemRechtType = {
+  readonly name: RollenSystemRechtEnum;
+  readonly isTechnical: boolean;
+};
