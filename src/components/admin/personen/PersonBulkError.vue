0caf9cd49f3952fd10e7ba9e1211a48101bafade
Yannick Clausen <yannick.clausen@capgemini.com>
2026-03-26 14:40:39 +0100

    refactor: extract BulkErrorDialog into component and refactor use in parents

--- src/components/admin/personen/PersonBulkError.vue
+++ src/components/admin/personen/PersonBulkError.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import LayoutCard from '@/components/cards/LayoutCard.vue';
+  import BulkErrorDialog from '@/components/dialog/BulkErrorDialog.vue';
   import { buildCSV, download } from '@/utils/file';
   import { ref, type Ref } from 'vue';
   import { useI18n, type Composer } from 'vue-i18n';
@@ -9,7 +9,6 @@
   const { mdAndDown }: { mdAndDown: Ref<boolean> } = useDisplay();
 
   const progress: Ref<number> = ref<number>(0);
-  const closeBulkErrorDialogDialogVisible: Ref<boolean> = ref(false);
 
   type ErrorEntry = {
     error: string;
@@ -30,244 +29,107 @@
     (event: 'update:isDialogVisible', isDialogVisible: boolean): void;
   };
 
-  type CSVHeaders = 'Vorname' | 'Nachname' | 'Benutzername' | 'Fehlermeldung';
-  type CSVRow = Record<CSVHeaders, string | undefined>;
-
   const props: Props = defineProps<Props>();
   const emit: Emits = defineEmits<Emits>();
 
   const showBulkErrorDialog: Ref<boolean> = ref(props.isDialogVisible);
 
-  // Saves the errors as a text file
   function saveErrorsAsCSV(): void {
+    type CSVHeaders = 'Vorname' | 'Nachname' | 'Benutzername' | 'Fehlermeldung';
+    type CSVRow = Record<CSVHeaders, string | undefined>;
     const headers: CSVHeaders[] = ['Vorname', 'Nachname', 'Benutzername', 'Fehlermeldung'];
-
     const fileName: string = `Fehler-Mehrfachbearbeitung-${new Date().toLocaleDateString('de-DE').replace(/\./g, '-')}.txt`;
-
     const csvRows: CSVRow[] = props.errors.map((entry: ErrorEntry) => ({
       Vorname: entry.vorname,
       Nachname: entry.nachname,
       Benutzername: entry.username,
       Fehlermeldung: entry.error,
     }));
-
     const actionIntroText: string = t('admin.person.bulk.bulkErrorMessageShort', {
       bulkOperationName: props.bulkOperationName,
     });
-
     const csvContent: string = buildCSV<CSVHeaders>(headers, csvRows, actionIntroText);
-
     const blob: Blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-
     download(fileName, blob);
   }
 
-  function closeBulkErrorDialog(): void {
+  function closeBulkErrorDialog(isVisible: boolean): void {
     progress.value = 0;
-    showBulkErrorDialog.value = false;
-    emit('update:isDialogVisible', false);
+    showBulkErrorDialog.value = isVisible;
+    emit('update:isDialogVisible', isVisible);
+  }
+
+  function errorToDisplayText(entry: ErrorEntry): string {
+    return `${entry.vorname} ${entry.nachname} (${entry.username})`;
+  }
+
+  function errorToAlertText(entry: ErrorEntry): string {
+    return entry.error;
   }
 </script>
 
 <template>
-  <v-dialog
-    v-model="showBulkErrorDialog"
-    persistent
+  <BulkErrorDialog
+    :is-dialog-visible="props.isDialogVisible"
+    :errors="props.errors"
+    :error-to-display-text="errorToDisplayText"
+    :error-to-alert-text="errorToAlertText"
+    :dialog-title="
+      props.bulkOperationName === t('admin.person.resetPassword') && props.passwords
+        ? t('admin.person.resetPassword')
+        : $t('admin.person.bulk.bulkErrorTitle')
+    "
+    :close-confirmation-text="t('admin.person.bulk.closeBulkErrorDialogConfirmationMessage')"
+    :testIdBase="'person-bulk-error'"
+    :download-button-text="t('admin.person.bulk.saveList')"
+    :close-button-text="t('admin.person.bulk.closeList')"
+    @update:isDialogVisible="closeBulkErrorDialog"
+    @download="saveErrorsAsCSV"
   >
-    <LayoutCard
-      data-testid="person-bulk-error-layout-card"
-      :closable="false"
-      :header="
-        props.bulkOperationName === t('admin.person.resetPassword') && passwords
-          ? t('admin.person.resetPassword')
-          : $t('admin.person.bulk.bulkErrorTitle')
-      "
-      @on-close-clicked="closeBulkErrorDialogDialogVisible = true"
-    >
-      <!-- This container is scrollable because of the max height -->
-      <v-container style="max-height: 60vh; overflow-y: auto">
-        <template v-if="props.bulkOperationName === t('admin.person.resetPassword') && passwords">
-          <!-- Success message and download -->
-          <v-row
-            class="my-4"
-            justify="center"
-          >
-            <v-col cols="auto">
-              <v-icon
-                small
-                color="#1EAE9C"
-                icon="mdi-check-circle"
-              />
-            </v-col>
-          </v-row>
-          <p class="mt-2 text-center">
-            {{ t('admin.person.bulk.bulkPasswordReset.success') }}
-          </p>
-          <v-row class="mt-4 justify-center">
-            <v-col
-              cols="12"
-              sm="6"
-              md="auto"
-            >
-              <v-btn
-                :block="mdAndDown"
-                class="primary"
-                data-testid="person-bulk-error-download-passwords-button"
-                @click="() => passwords && download(filename ?? 'default-filename.csv', passwords)"
-              >
-                {{ t('admin.person.bulk.bulkPasswordReset.downloadResult') }}
-              </v-btn>
-            </v-col>
-          </v-row>
-          <v-divider
-            class="border-opacity-100 rounded my-8"
-            thickness="5px"
-          />
-        </template>
-
-        <!-- Error list -->
-        <p
-          v-if="props.bulkOperationName === t('admin.person.resetPassword') && passwords"
-          class="headline-2 mb-4"
-        >
-          {{ $t('admin.person.bulk.bulkErrorTitle') }}
-        </p>
-        <p class="text-body bold pre-line">
-          {{
-            t('admin.person.bulk.bulkErrorMessage', {
-              bulkOperationName: props.bulkOperationName,
-            })
-          }}
-        </p>
-        <ol>
-          <li
-            v-for="(entry, index) in props.errors"
-            :key="index"
-            class="text-body font-weight-bold my-2 ml-4"
-          >
-            <div>
-              <span class="text-body font-weight-bold"
-                >{{ entry.vorname }} {{ entry.nachname }} ({{ entry.username }})</span
-              >
-            </div>
-            <div class="ml-4 text-body">
-              <v-alert
-                class="py-2 px-2"
-                type="error"
-              >
-                {{ entry.error }}
-              </v-alert>
-            </div>
-          </li>
-        </ol>
-      </v-container>
-
-      <!-- Actions remain fixed -->
-      <v-card-actions
-        class="justify-center"
-        style="border-top: 1px solid #e5eaef"
+    <template v-if="props.bulkOperationName === t('admin.person.resetPassword') && props.passwords">
+      <v-row
+        class="my-4"
+        justify="center"
       >
-        <v-row class="py-3 px-2 justify-center">
-          <v-spacer class="hidden-sm-and-down" />
-
-          <v-col
-            cols="12"
-            sm="6"
-            md="auto"
+        <v-col cols="auto">
+          <v-icon
+            small
+            color="#1EAE9C"
+            icon="mdi-check-circle"
+          />
+        </v-col>
+      </v-row>
+      <p class="mt-2 text-center">
+        {{ t('admin.person.bulk.bulkPasswordReset.success') }}
+      </p>
+      <v-row class="mt-4 justify-center">
+        <v-col
+          cols="12"
+          sm="6"
+          md="auto"
+        >
+          <v-btn
+            :block="mdAndDown"
+            class="primary"
+            data-testid="person-bulk-error-download-passwords-button"
+            @click="() => props.passwords && download(props.filename ?? 'default-filename.csv', props.passwords)"
           >
-            <v-btn
-              :block="mdAndDown"
-              class="secondary"
-              data-testid="person-bulk-error-discard-button"
-              @click="closeBulkErrorDialogDialogVisible = true"
-            >
-              {{ t('admin.person.bulk.closeList') }}
-            </v-btn>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="6"
-            md="auto"
-          >
-            <v-btn
-              :block="mdAndDown"
-              class="primary"
-              data-testid="person-bulk-error-save-button"
-              @click="saveErrorsAsCSV"
-            >
-              {{ t('admin.person.bulk.saveList') }}
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-card-actions>
-    </LayoutCard>
-  </v-dialog>
-
-  <v-dialog
-    v-model="closeBulkErrorDialogDialogVisible"
-    persistent
-    max-width="900"
-  >
-    <LayoutCard
-      :header="$t('admin.person.bulk.bulkErrorTitle')"
-      @on-close-clicked="closeBulkErrorDialogDialogVisible = false"
-    >
-      <v-card-text>
-        <v-container class="my-4">
-          <v-row
-            class="text-body text-error justify-center"
-            align="center"
-          >
-            <v-col
-              class="text-center"
-              cols="12"
-            >
-              <v-icon
-                class="mr-2 pb-1"
-                icon="mdi-alert"
-              />
-              <span>
-                {{ t('admin.person.bulk.closeBulkErrorDialogConfirmationMessage') }}
-              </span>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-      <v-card-actions class="justify-center">
-        <v-row class="justify-center">
-          <v-col
-            cols="12"
-            sm="6"
-            md="4"
-          >
-            <v-btn
-              :block="mdAndDown"
-              class="secondary"
-              data-testid="cancel-close-bulk-error-dialog-button"
-              @click.stop="closeBulkErrorDialogDialogVisible = false"
-            >
-              {{ $t('cancel') }}
-            </v-btn>
-          </v-col>
-          <v-col
-            cols="12"
-            sm="6"
-            md="4"
-          >
-            <v-btn
-              :block="mdAndDown"
-              class="primary"
-              data-testid="confirm-close-bulk-error-dialog-button"
-              @click.stop="closeBulkErrorDialog"
-            >
-              {{ $t('close') }}
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-card-actions>
-    </LayoutCard>
-  </v-dialog>
+            {{ t('admin.person.bulk.bulkPasswordReset.downloadResult') }}
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-divider
+        class="border-opacity-100 rounded my-8"
+        thickness="5px"
+      />
+      <p class="headline-2 mb-4">
+        {{ $t('admin.person.bulk.bulkErrorTitle') }}
+      </p>
+    </template>
+    <p class="text-body bold pre-line">
+      {{ t('admin.person.bulk.bulkErrorMessage', { bulkOperationName: props.bulkOperationName }) }}
+    </p>
+  </BulkErrorDialog>
 </template>
 
 <style scoped>
--- src/components/admin/service-provider/RollenerweiterungAssignErrorDialog.vue
+++ src/components/admin/service-provider/RollenerweiterungAssignErrorDialog.vue
@@ -1,203 +1,68 @@
 <script setup lang="ts">
-  import LayoutCard from '@/components/cards/LayoutCard.vue';
+  import BulkErrorDialog from '@/components/dialog/BulkErrorDialog.vue';
   import { buildCSV, download } from '@/utils/file';
-  import { ref, type Ref } from 'vue';
   import { useI18n, type Composer } from 'vue-i18n';
-  import { useDisplay } from 'vuetify';
   import type { RollenerweiterungAssignErrorDialogProps } from './types';
 
   const { t }: Composer = useI18n({ useScope: 'global' });
-  const { mdAndDown }: { mdAndDown: Ref<boolean> } = useDisplay();
-
-  const confirmCloseDialog: Ref<boolean> = ref(false);
 
   type Emits = {
     (event: 'update:isDialogVisible', isDialogVisible: boolean): void;
   };
 
-  type CSVHeaders = 'Dienststellennummer' | 'Angebot' | 'Rolle' | 'Fehlermeldung';
-  type CSVRow = Record<CSVHeaders, string | undefined>;
-
   const props: RollenerweiterungAssignErrorDialogProps = defineProps<RollenerweiterungAssignErrorDialogProps>();
   const emit: Emits = defineEmits<Emits>();
 
-  // Saves the errors as a text file
   function saveErrorsAsCSV(): void {
     if (!props.errors || props.errors.length === 0) {
       return;
     }
+    type CSVHeaders = 'Dienststellennummer' | 'Angebot' | 'Rolle' | 'Fehlermeldung';
+    type CSVRow = Record<CSVHeaders, string | undefined>;
     const headers: CSVHeaders[] = ['Dienststellennummer', 'Angebot', 'Rolle', 'Fehlermeldung'];
     const fileName: string = `Fehler-Rollenerweiterung-${new Date().toLocaleDateString('de-DE').replaceAll('.', '-')}.txt`;
-
     const csvRows: CSVRow[] = props.errors?.map((entry: RollenerweiterungAssignErrorDialogProps['errors'][number]) => ({
       Dienststellennummer: props.dstNr,
       Angebot: props.serviceProviderName,
       Rolle: entry.rolle,
       Fehlermeldung: entry.message,
     }));
-
     const csvContent: string = buildCSV<CSVHeaders>(
       headers,
       csvRows,
       t('angebot.schulspezifischeErweiterungen.errorCsvIntroText'),
     );
-
     const blob: Blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-
     download(fileName, blob);
   }
 
-  function closeBulkErrorDialog(): void {
-    confirmCloseDialog.value = false;
-    emit('update:isDialogVisible', false);
+  function errorToDisplayText(entry: RollenerweiterungAssignErrorDialogProps['errors'][number]): string {
+    return `${props.dstNr} ${props.serviceProviderName} (${entry.rolle})`;
+  }
+
+  function errorToAlertText(entry: RollenerweiterungAssignErrorDialogProps['errors'][number]): string {
+    return entry.message;
   }
 </script>
 
 <template>
-  <v-dialog
-    :model-value="props.isDialogVisible"
-    persistent
+  <BulkErrorDialog
+    :is-dialog-visible="props.isDialogVisible"
+    :errors="props.errors"
+    :error-to-display-text="errorToDisplayText"
+    :error-to-alert-text="errorToAlertText"
+    :dialog-title="t('angebot.schulspezifischeErweiterungen.assignErrorTitle')"
+    :close-confirmation-text="t('angebot.schulspezifischeErweiterungen.closeConfirmationMessage')"
+    :testIdBase="'rollenerweiterung-error'"
+    :download-button-text="t('angebot.schulspezifischeErweiterungen.saveList')"
+    :close-button-text="t('angebot.schulspezifischeErweiterungen.closeList')"
+    @update:isDialogVisible="emit('update:isDialogVisible', $event)"
+    @download="saveErrorsAsCSV"
   >
-    <LayoutCard
-      data-testid="rollenerweiterung-error-layout-card"
-      :closable="false"
-      :header="t('angebot.schulspezifischeErweiterungen.assignErrorTitle')"
-      @on-close-clicked="confirmCloseDialog = true"
-    >
-      <v-container>
-        <p class="text-body bold pre-line">
-          {{ t('angebot.schulspezifischeErweiterungen.assignErrorIntroText') }}
-        </p>
-        <!-- Error list -->
-        <ol>
-          <li
-            v-for="(entry, index) in props.errors"
-            :key="index"
-            class="text-body font-weight-bold my-2 ml-4"
-          >
-            <div>
-              <span class="text-body font-weight-bold"
-                >{{ props.dstNr }} {{ props.serviceProviderName }} ({{ entry.rolle }})</span
-              >
-            </div>
-            <div class="ml-4 text-body">
-              <v-alert
-                class="py-2 px-2"
-                type="error"
-              >
-                {{ entry.message }}
-              </v-alert>
-            </div>
-          </li>
-        </ol>
-      </v-container>
-
-      <!-- Actions remain fixed -->
-      <v-card-actions
-        class="justify-center"
-        style="border-top: 1px solid #e5eaef"
-      >
-        <v-row class="py-3 px-2 justify-center">
-          <v-spacer class="hidden-sm-and-down" />
-
-          <v-col
-            cols="12"
-            sm="6"
-            md="auto"
-          >
-            <v-btn
-              :block="mdAndDown"
-              class="secondary"
-              data-testid="rollenerweiterung-error-discard-button"
-              @click="confirmCloseDialog = true"
-            >
-              {{ t('angebot.schulspezifischeErweiterungen.closeList') }}
-            </v-btn>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="6"
-            md="auto"
-          >
-            <v-btn
-              :block="mdAndDown"
-              class="primary"
-              data-testid="rollenerweiterung-error-save-button"
-              @click="saveErrorsAsCSV"
-            >
-              {{ t('angebot.schulspezifischeErweiterungen.saveList') }}
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-card-actions>
-    </LayoutCard>
-  </v-dialog>
-
-  <v-dialog
-    v-model="confirmCloseDialog"
-    persistent
-    max-width="900"
-  >
-    <LayoutCard
-      :header="t('angebot.schulspezifischeErweiterungen.assignErrorTitle')"
-      @on-close-clicked="confirmCloseDialog = false"
-    >
-      <v-card-text>
-        <v-container class="my-4">
-          <v-row
-            class="text-body text-error justify-center"
-            align="center"
-          >
-            <v-col
-              class="text-center"
-              cols="12"
-            >
-              <v-icon
-                class="mr-2 pb-1"
-                icon="mdi-alert"
-              />
-              <span>
-                {{ t('angebot.schulspezifischeErweiterungen.closeConfirmationMessage') }}
-              </span>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-      <v-card-actions class="justify-center">
-        <v-row class="justify-center">
-          <v-col
-            cols="12"
-            sm="6"
-            md="4"
-          >
-            <v-btn
-              :block="mdAndDown"
-              class="secondary"
-              data-testid="cancel-close-bulk-error-dialog-button"
-              @click.stop="confirmCloseDialog = false"
-            >
-              {{ $t('cancel') }}
-            </v-btn>
-          </v-col>
-          <v-col
-            cols="12"
-            sm="6"
-            md="4"
-          >
-            <v-btn
-              :block="mdAndDown"
-              class="primary"
-              data-testid="confirm-close-bulk-error-dialog-button"
-              @click.stop="closeBulkErrorDialog"
-            >
-              {{ $t('close') }}
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-card-actions>
-    </LayoutCard>
-  </v-dialog>
+    <p class="text-body bold pre-line">
+      {{ t('angebot.schulspezifischeErweiterungen.assignErrorIntroText') }}
+    </p>
+  </BulkErrorDialog>
 </template>
 
 <style scoped>
