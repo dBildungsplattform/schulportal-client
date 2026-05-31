272e1c182f5fd1125188534588fc2bff939fc455
godtierbatuhan <boucharay1001@gmail.com>
2026-05-31 16:23:52 +0200

    Added back globals

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,8 +1,8 @@
 import { config } from '@vue/test-utils';
 import { I18n, createI18n } from 'vue-i18n';
 import { createVuetify } from 'vuetify';
+import { beforeAll } from 'vitest';
 import { TestingPinia, createTestingPinia } from '@pinia/testing';
-// MSW will probably be removed soon anyways
 import de_locales from './src/locales/de-DE.json';
 
 const i18n: I18n = createI18n({
@@ -14,9 +14,25 @@ const i18n: I18n = createI18n({
   },
 });
 
-// eslint-disable-next-line @typescript-eslint/typedef
-const vuetify = createVuetify({});
-
+const vuetify: ReturnType<typeof createVuetify> = createVuetify({});
 const pinia: TestingPinia = createTestingPinia();
+
+beforeAll(() => {
+  // @ts-expect-error: global has any type
+  global.ResizeObserver = class ResizeObserver {
+    public observe(): void {
+      /* empty */
+    }
+    public unobserve(): void {
+      /* empty */
+    }
+    public disconnect(): void {
+      /* empty */
+    }
+  };
+  // Fix Vuetify >=3.8.7 overlay bug (visualViewport not defined)
+  // @ts-expect-error: visualViewport is missing in Node
+  global.visualViewport = new EventTarget();
+});
 
 config.global.plugins = [i18n, pinia, vuetify];
