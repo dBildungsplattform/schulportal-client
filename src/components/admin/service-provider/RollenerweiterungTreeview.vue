d554c9a1b52ce8f99feea8051f9d59320036aa43
godtierbatuhan <boucharay1001@gmail.com>
2026-07-28 00:37:29 +0200

    Typing merkmale as array instead of set

--- src/components/admin/service-provider/RollenerweiterungTreeview.vue
+++ src/components/admin/service-provider/RollenerweiterungTreeview.vue
@@ -9,7 +9,7 @@
     id: string;
     name: string;
     rollenart: RollenArt;
-    merkmale?: Set<RollenMerkmal>;
+    merkmale?: RollenMerkmal[];
   };
 
   type TreeNode = {
@@ -73,7 +73,7 @@
     {
       key: 'MPT',
       labelKey: 'angebot.groupMPT',
-      match: (r: RolleForSelection) => Array.isArray(r.merkmale) && r.merkmale.includes(RollenMerkmal.MptRolle),
+      match: (r: RolleForSelection) => r.merkmale?.includes(RollenMerkmal.MptRolle) ?? false,
     },
   ];
 
