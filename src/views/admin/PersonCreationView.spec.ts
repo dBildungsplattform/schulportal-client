1e74094dc40d28920195037aed5439a78175eee3
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2026-08-31 16:40:47 +0200

    Fix flaky test

--- src/views/admin/PersonCreationView.spec.ts
+++ src/views/admin/PersonCreationView.spec.ts
@@ -474,8 +474,8 @@ describe('PersonCreationView', () => {
     // Form is resetting after submit so orga should be undefined
     expect(selectors.organisationsebeneSelect?.vm.$data).toStrictEqual({});
 
+    await vi.waitFor(() => expect(spy).toHaveBeenCalledOnce());
     await nextTick();
-    expect(spy).toHaveBeenCalledOnce();
     expect(wrapper?.find('[data-testid="person-success-text"]').isVisible()).toBe(true);
   });
 
